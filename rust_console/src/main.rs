245452698614706a953853be710ceb2805508693
Jeff Price <jeffrey.thomas.price@gmail.com>
2023-02-24 08:17:57 -0500

    .

--- rust_console/src/main.rs
+++ rust_console/src/main.rs
@@ -1,219 +1,108 @@
+mod console;
+
 use std::{
     error::Error,
-    fmt::{Debug, Display},
     io::{self, Write},
 };
 
-#[derive(Debug)]
-struct ReturnCodeError {
-    operation: String,
-    return_code: i32,
-}
+use console::*;
 
-impl Display for ReturnCodeError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "ReturnCodeError {{ operation = {:?}, return_code = {:?} }}",
-            self.operation, self.return_code
-        )
+fn print_at(mut position: Position, s: &str) -> Result<(), io::Error> {
+    for line in s.lines().into_iter() {
+        set_cursor_position(position)?;
+        io::stdout().write(line.as_bytes())?;
+        position.y += 1;
     }
-}
-
-impl Error for ReturnCodeError {}
-
-#[derive(Debug)]
-struct ExpectedValueError<ExpectedType, ActualType> {
-    expected: ExpectedType,
-    actual: ActualType,
-}
-
-impl<ExpectedType, ActualType> Display for ExpectedValueError<ExpectedType, ActualType>
-where
-    ExpectedType: Debug,
-    ActualType: Debug,
-{
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "ExpectedValueError {{ expected = {:?}, actual = {:?} }}",
-            self.expected, self.actual
-        )
-    }
-}
-
-impl<ExpectedType, ActualType> std::error::Error for ExpectedValueError<ExpectedType, ActualType>
-where
-    ExpectedType: Debug,
-    ActualType: Debug,
-{
-}
-
-fn get_window_size() -> Result<libc::winsize, ReturnCodeError> {
-    let result = libc::winsize {
-        ws_row: 0,
-        ws_col: 0,
-        ws_xpixel: 0,
-        ws_ypixel: 0,
-    };
-    unsafe {
-        match libc::ioctl(libc::STDIN_FILENO, libc::TIOCGWINSZ, &result) {
-            0 => Ok(result),
-            return_code => Err(ReturnCodeError {
-                operation: "ioctl TIOCGWINSZ".to_string(),
-                return_code,
-            }),
-        }
-    }
-}
-
-#[derive(Debug)]
-struct Position {
-    row: u16,
-    column: u16,
-}
-
-fn get_cursor_position() -> Result<Position, Box<dyn Error>> {
-    io::stdout().write(&[0x1b, b'[', b'6', b'n'])?;
-    io::stdout().flush()?;
-
-    wait_for_u8(0x1b);
-    expect_u8(b'[')?;
-    let row = std::str::from_utf8(expect_until_u8(b';', 4)?.as_ref())?.parse::<u16>()?;
-    let column = std::str::from_utf8(expect_until_u8(b'R', 4)?.as_ref())?.parse::<u16>()?;
-    Ok(Position { row, column })
-}
-
-fn set_cursor_position(p: Position) -> Result<(), io::Error> {
-    io::stdout().write(&[0x1b])?;
-    io::stdout().write(format!("[{};{}H", p.row, p.column).as_bytes())?;
     io::stdout().flush()?;
     Ok(())
 }
 
-fn wait_for_u8(value: u8) {
-    loop {
-        let result: Result<u8, _> = unsafe { libc::getchar() }.try_into();
-        match result {
-            Ok(result) => {
-                if result == value {
-                    return;
-                }
-            }
-            Err(_) => continue,
-        }
-    }
+struct RectangleStyle {
+    interior: u8,
+    left: u8,
+    right: u8,
+    top: u8,
+    bottom: u8,
+    top_left: u8,
+    top_right: u8,
+    bottom_left: u8,
+    bottom_right: u8,
 }
 
-fn expect_u8(expected: u8) -> Result<(), ExpectedValueError<u8, i32>> {
-    let actual_i32 = unsafe { libc::getchar() };
-    let actual_u8: Result<u8, _> = actual_i32.try_into();
-    match actual_u8 {
-        Ok(actual_u8) => {
-            if actual_u8 == expected {
-                Ok(())
+fn draw_rect(position: Position, size: Size, style: RectangleStyle) -> Result<(), Box<dyn Error>> {
+    let mut content = Vec::with_capacity(
+        (
+            // the size of the rectangle
+            size.width * size.height + 
+        // plus the newlines between rows
+        (size.height - 1)
+        )
+        .into(),
+    );
+    for y in position.y..(position.y + size.height) {
+        let is_top = y == position.y;
+        let is_bottom = y == position.y + size.height - 1;
+        for x in position.x..(position.x + size.width) {
+            let is_left = x == position.x;
+            let is_right = x == position.x + size.width - 1;
+            content.push(if is_top && is_left {
+                style.top_left
+            } else if is_top && is_right {
+                style.top_right
+            } else if is_top {
+                style.top
+            } else if is_bottom && is_left {
+                style.bottom_left
+            } else if is_bottom && is_right {
+                style.bottom_right
+            } else if is_bottom {
+                style.bottom
+            } else if is_left {
+                style.left
+            } else if is_right {
+                style.right
             } else {
-                Err(ExpectedValueError {
-                    expected,
-                    actual: actual_i32,
-                })
-            }
-        }
-        Err(_) => Err(ExpectedValueError {
-            expected,
-            actual: actual_i32,
-        }),
-    }
-}
-
-fn expect_until_u8(expected: u8, max: usize) -> Result<Vec<u8>, ExpectedValueError<u8, i32>> {
-    let mut results = Vec::new();
-    for _ in 0..max {
-        let next_i32 = unsafe { libc::getchar() };
-        let next_u8: Result<u8, _> = next_i32.try_into();
-        match next_u8 {
-            Ok(next_u8) => {
-                if next_u8 == expected {
-                    break;
-                } else {
-                    results.push(next_u8);
-                }
-            }
-            _ => Err(ExpectedValueError {
-                expected: expected,
-                actual: next_i32,
-            })?,
+                style.interior
+            });
         }
     }
-    Ok(results)
-}
-
-fn set_raw_mode() -> Result<(), ReturnCodeError> {
-    // set raw mode
-    // https://github.com/wertarbyte/coreutils/blob/master/src/stty.c#L1180
-    let mut termios = get_termios()?;
-    termios.c_iflag = 0;
-    termios.c_oflag &= !libc::OPOST;
-    termios.c_lflag &= !(libc::ISIG | libc::ICANON | libc::ECHO);
-    termios.c_cc[libc::VMIN] = 1;
-    termios.c_cc[libc::VTIME] = 0;
-    set_termios(termios, libc::TCSADRAIN)?;
+    print_at(position, std::str::from_utf8(&content)?)?;
     Ok(())
-}
-
-fn set_cooked_mode() -> Result<(), ReturnCodeError> {
-    // set cooked mode
-    // https://github.com/wertarbyte/coreutils/blob/master/src/stty.c#L1167
-    let mut termios = get_termios()?;
-    termios.c_iflag |= libc::BRKINT | libc::IGNPAR | libc::ISTRIP | libc::ICRNL | libc::IXON;
-    termios.c_oflag |= libc::OPOST;
-    termios.c_lflag |= libc::ISIG | libc::ICANON | libc::ECHO;
-    set_termios(termios, libc::TCSANOW)?;
-    Ok(())
-}
-
-fn get_termios() -> Result<libc::termios, ReturnCodeError> {
-    let mut result = libc::termios {
-        c_iflag: 0,
-        c_oflag: 0,
-        c_cflag: 0,
-        c_lflag: 0,
-        c_line: 0,
-        c_cc: [0; 32],
-        c_ispeed: 0,
-        c_ospeed: 0,
-    };
-    match unsafe { libc::tcgetattr(libc::STDIN_FILENO, &mut result) } {
-        0 => Ok(result),
-        return_code => Err(ReturnCodeError {
-            operation: "get termios".to_string(),
-            return_code,
-        }),
-    }
-}
-
-fn set_termios(value: libc::termios, optional_actions: libc::c_int) -> Result<(), ReturnCodeError> {
-    match unsafe { libc::tcsetattr(libc::STDIN_FILENO, optional_actions, &value) } {
-        0 => Ok(()),
-        return_code => Err(ReturnCodeError {
-            operation: "set termios".to_string(),
-            return_code,
-        }),
-    }
 }
 
 fn main() {
     fn do_it() -> Result<(), Box<dyn Error>> {
         set_raw_mode()?;
 
-        let winsize = get_window_size()?;
-        println!("winsize = {} x {}\r", winsize.ws_col, winsize.ws_row);
+        draw_rect(
+            Position { x: 0, y: 0 },
+            get_window_size()?,
+            RectangleStyle {
+                interior: b' ',
+                left: b'<',
+                right: b'>',
+                top: b'^',
+                bottom: b'v',
+                top_left: b'1',
+                top_right: b'2',
+                bottom_left: b'3',
+                bottom_right: b'4',
+            },
+        )?;
 
-        let cursor = get_cursor_position()?;
-        println!("cursor = {:?}\r", cursor);
+        print_at(
+            Position { x: 5, y: 4 },
+            format!("window size = {}\n", get_window_size()?).as_str(),
+        )?;
+        print_at(
+            Position { x: 5, y: 5 },
+            format!("cursor = {}\n", get_cursor_position()?).as_str(),
+        )?;
 
-        set_cursor_position(Position { row: 4, column: 5 })?;
-        println!("moved cursor!");
+        print_at(
+            Position { x: 10, y: 7 },
+            format!("multi-line output\nfoo\nbar\n\nbaz").as_str(),
+        )?;
 
         Ok(())
     }
