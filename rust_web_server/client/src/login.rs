a1dcb94617de5192d641992c3d1457f37f93b3d8
Jeff Price <jeffrey.thomas.price@gmail.com>
2023-02-28 20:40:29 -0500

    functional components are cool

--- rust_web_server/client/src/login.rs
+++ rust_web_server/client/src/login.rs
@@ -4,102 +4,76 @@ use log::*;
 use shared::auth;
 use wasm_bindgen::{JsCast, JsValue};
 use wasm_bindgen_futures::JsFuture;
-use web_sys::{Request, RequestInit, Response, Storage};
-use yew::{html, Component, Context, Html, InputEvent, SubmitEvent};
+use web_sys::{Request, RequestInit, Response};
+use yew::{function_component, html, use_state, Callback, Html, InputEvent, SubmitEvent};
 
-pub struct Login {
-    username: String,
-    password: String,
-}
-
-pub enum Message {
-    Noop,
-    Submit,
-    UsernameUpdate(String),
-    PasswordUpdate(String),
-}
-
-impl Component for Login {
-    type Message = Message;
-
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self {
-            username: "".to_string(),
-            password: "".to_string(),
-        }
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        let username_onchange =
-            ctx.link()
-                .callback(|e: InputEvent| match get_value_from_input_element(e) {
-                    Some(x) => Self::Message::UsernameUpdate(x),
-                    None => Self::Message::Noop,
-                });
-
-        let password_onchange =
-            ctx.link()
-                .callback(|e: InputEvent| match get_value_from_input_element(e) {
-                    Some(x) => Self::Message::PasswordUpdate(x),
-                    None => Self::Message::Noop,
-                });
-
-        html! {
-            <form
-                onsubmit={ctx.link().callback(|e: SubmitEvent| {
-                    e.prevent_default();
-                    Self::Message::Submit
-                })}
-            >
-                <div class="row">
-                    <div class="col">
-                        <label for="username">{ "Username" }</label>
-                        <input name="username" type="text" oninput={username_onchange} />
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col">
-                        <label for="password">{ "Password" }</label>
-                        <input name="password" type="password" oninput={password_onchange} />
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col">
-                        <button type="submit" class="button primary">{ "Login" }</button>
-                    </div>
-                </div>
-            </form>
-        }
-    }
-
-    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Message::Noop => false,
-            Message::Submit => {
-                debug!(
-                    "TODO JEFF submit, username = {}, password = {}",
-                    self.username, self.password
-                );
-                let username = self.username.clone();
-                let password = self.password.clone();
-                ctx.link().send_future(async move {
-                    let response = login(&username, &password).await;
-                    debug!("TODO login complete, should be sending a success or failure message");
-                    Message::Noop
-                });
-                false
+#[function_component]
+pub fn Login() -> Html {
+    let username = use_state(|| "".to_string());
+    let username_changed = {
+        let state = username.clone();
+        Callback::from(move |e: InputEvent| {
+            if let Some(x) = get_value_from_input_element(e) {
+                state.set(x)
             }
-            Message::UsernameUpdate(value) => {
-                self.username = value;
-                false
+        })
+    };
+
+    let password = use_state(|| "".to_string());
+    let password_changed = {
+        let state = password.clone();
+        Callback::from(move |e: InputEvent| {
+            if let Some(x) = get_value_from_input_element(e) {
+                state.set(x)
             }
-            Message::PasswordUpdate(value) => {
-                self.password = value;
-                false
-            }
-        }
+        })
+    };
+
+    let submit = {
+        let username = username.clone();
+        let password = password.clone();
+        Callback::from(move |e: SubmitEvent| {
+            e.prevent_default();
+            debug!(
+                "TODO JEFF submit, username = {}, password = {}",
+                *username, *password
+            );
+
+            let username = username.clone();
+            let password = password.clone();
+            wasm_bindgen_futures::spawn_local(async move {
+                match login(&username, &password).await {
+                    Ok(_) => debug!(
+                        "TODO login complete, should be sending a success or failure message"
+                    ),
+                    Err(e) => error!("TODO JEFF login error: {e:?}"),
+                }
+            });
+        })
+    };
+
+    html! {
+        <form
+            onsubmit={submit}
+        >
+            <div class="row">
+                <div class="col">
+                    <label for="username">{ "Username" }</label>
+                    <input name="username" type="text" oninput={username_changed} />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <label for="password">{ "Password" }</label>
+                    <input name="password" type="password" oninput={password_changed} />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <button type="submit" class="button primary">{ "Login" }</button>
+                </div>
+            </div>
+        </form>
     }
 }
 
