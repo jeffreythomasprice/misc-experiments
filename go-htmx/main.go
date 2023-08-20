7b65f6f73ced9a92445151532a1d086f00b160a7
Jeff Price <j.price@servicepower.com>
2023-08-20 18:11:08 -0400

    .

--- go-htmx/main.go
+++ go-htmx/main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"fmt"
-	"html/template"
-	"io"
 	"net/http"
+	"net/url"
 	"os"
 	"path"
-	"strings"
 
 	"log/slog"
 
@@ -36,14 +34,37 @@ func main() {
 	}
 	r.Handle("/*", http.FileServer(http.Dir(webDir)))
 
-	count := 0
-	testTemplate := mustTemplateFunc[int]("<div>{{ . }}</div>")
-	r.HandleFunc("/api/test", testTemplate.CreateHttpHandler(func() (int, error) {
-		// time.Sleep(time.Second * 5)
-		result := count
-		count += 1
-		return result, nil
-	}))
+	type MessageRequest struct {
+		Message string
+	}
+	validateMessage := func(form url.Values) (*MessageRequest, error) {
+		msg, err := ValidateSingleFormField(
+			form,
+			"message",
+			MinStringLength(1),
+		)
+		if err != nil {
+			return nil, err
+		}
+		return &MessageRequest{
+			Message: msg,
+		}, nil
+	}
+	r.Post(
+		"/api/message",
+		mustTemplateFunc[interface{}](`<li>
+			TODO JEFF does this template do anything?
+		</li>`).
+			CreateHttpFormHandler(func(r *http.Request, form url.Values) (interface{}, error) {
+				msg, err := validateMessage(form)
+				if err != nil {
+					return nil, err
+				}
+				slog.Info("TODO JEFF", "msg", msg)
+
+				return nil, nil
+			}),
+	)
 
 	addr := "127.0.0.1"
 	port := 8000
@@ -59,54 +80,4 @@ func getWebDir() (string, error) {
 		return "", err
 	}
 	return path.Join(cwd, "web"), nil
-}
-
-type TemplateFunc[T any] func(data T) (string, error)
-
-func newTemplateFunc[T any](text string) (TemplateFunc[T], error) {
-	t, err := template.New("").Parse(text)
-	if err != nil {
-		return nil, err
-	}
-	return func(data T) (string, error) {
-		var s strings.Builder
-		if err := t.Execute(&s, data); err != nil {
-			return "", err
-		}
-		return s.String(), nil
-	}, nil
-}
-
-func mustTemplateFunc[T any](text string) TemplateFunc[T] {
-	result, err := newTemplateFunc[T](text)
-	if err != nil {
-		panic(err)
-	}
-	return result
-}
-
-func (f TemplateFunc[T]) WriteTo(w io.Writer, data T) error {
-	text, err := f(data)
-	if err != nil {
-		return err
-	}
-	_, err = fmt.Fprint(w, text)
-	return err
-}
-
-func (f TemplateFunc[T]) CreateHttpHandler(dataFactory func() (T, error)) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		data, err := dataFactory()
-		if err != nil {
-			slog.Error("failed to generate data to render", "err", err)
-			w.WriteHeader(500)
-			return
-		}
-
-		if err := f.WriteTo(w, data); err != nil {
-			slog.Error("failed to render template", "err", err)
-			w.WriteHeader(500)
-			return
-		}
-	}
 }
