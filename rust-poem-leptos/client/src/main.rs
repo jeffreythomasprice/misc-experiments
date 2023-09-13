ddc6915d7180303cd5ea01800e4e83203d3a5bfb
Jeff Price <j.price@servicepower.com>
2023-09-12 21:01:31 -0400

    .

--- rust-poem-leptos/client/src/main.rs
+++ rust-poem-leptos/client/src/main.rs
@@ -1,10 +1,75 @@
-use leptos::{mount_to_body, spawn_local, view};
+use leptos::ev::KeyboardEvent;
+use leptos::*;
 use log::*;
 use shared::models::{ClientHelloRequest, ClientHelloResponse};
 use shared::websockets::{Message, WebSocketChannel};
 use std::error::Error;
 use std::panic;
+use std::rc::Rc;
 use wasm_bindgen::JsValue;
+
+#[component]
+pub fn Login<F>(cx: Scope, submit: F) -> impl IntoView
+where
+    F: Fn(String) + 'static,
+{
+    let (value, set_value) = create_signal(cx, "".to_string());
+
+    let on_submit = Rc::new(move || {
+        submit(value());
+    });
+
+    let on_button_click = {
+        let on_submit = on_submit.clone();
+        move |_| {
+            on_submit();
+        }
+    };
+
+    let on_input_keyup = move |e: KeyboardEvent| {
+        if e.key() == "Enter" {
+            on_submit();
+        }
+    };
+
+    let on_input_input = move |e| {
+        set_value(event_target_value(&e));
+    };
+
+    view! { cx,
+        <div>
+            <input type="text" autofocus on:keyup=on_input_keyup on:input=on_input_input />
+            <button on:click=on_button_click>Start</button>
+        </div>
+    }
+}
+
+#[component]
+pub fn App(cx: Scope) -> impl IntoView {
+    let (name, set_name) = create_signal(cx, None);
+
+    let login = move |name| {
+        debug!("TODO JEFF login: {name}");
+        set_name(Some(name));
+    };
+
+    let content = move || match name() {
+        Some(name) => view! { cx,
+            <>
+            <div>{name}</div>
+            </>
+        },
+        None => view! { cx,
+            <>
+            <Login submit=login/>
+            </>
+        },
+    };
+
+    view! { cx,
+        <>{content}</>
+    }
+}
 
 fn main() {
     console_log::init_with_level(Level::Debug).unwrap();
@@ -12,16 +77,16 @@ fn main() {
 
     spawn_local(async {
         if let Err(e) = test_request().await {
-            error!("error doing test request: {e:?}");
+            log::error!("error doing test request: {e:?}");
         }
         if let Err(e) = test_websockets().await {
-            error!("error doing test websockets: {e:?}");
+            log::error!("error doing test websockets: {e:?}");
         }
     });
 
     mount_to_body(|cx| {
         view! { cx,
-            <p>"Hello, world!"</p>
+            <App/>
         }
     })
 }
@@ -54,7 +119,7 @@ async fn test_websockets() -> Result<(), JsValue> {
                     "TODO JEFF got binary message from client, {} bytes",
                     value.len()
                 ),
-                Err(_e) => error!("TODO JEFF error from websocket"),
+                Err(_e) => log::error!("TODO JEFF error from websocket"),
             }
         }
     });
@@ -66,7 +131,7 @@ async fn test_websockets() -> Result<(), JsValue> {
             ))
             .await
         {
-            error!("TODO JEFF error sending test message: {e:?}");
+            log::error!("TODO JEFF error sending test message: {e:?}");
         }
     });
 
