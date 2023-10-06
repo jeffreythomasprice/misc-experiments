fb9e8515525378cf979adac0e2df43ed0b0325be
Jeff Price <j.price@servicepower.com>
2023-10-06 19:47:13 -0400

    .

--- go-echo-htmx/main.go
+++ go-echo-htmx/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"errors"
 	"log/slog"
 	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -14,6 +16,32 @@ import (
 	. "github.com/maragudk/gomponents/components"
 	. "github.com/maragudk/gomponents/html"
 )
+
+type usersService struct{}
+
+func (service *usersService) ValidateCredentials(username, password string) (bool, error) {
+	slog.Debug("checking credentials", "username", username)
+	result := password == "password"
+	slog.Debug("result", "username", username, "result", result)
+	return result, nil
+}
+
+func (service *usersService) IsLoggedInCookie(cookie *http.Cookie, err error) (bool, error) {
+	if err != nil {
+		if errors.Is(err, http.ErrNoCookie) {
+			slog.Debug("missing auth cookie")
+			return false, nil
+		}
+		return false, err
+	}
+	slog.Debug("TODO should check auth cookie", "cookie", cookie)
+	return true, nil
+}
+
+type serviceContext struct {
+	echo.Context
+	users *usersService
+}
 
 func main() {
 	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
@@ -29,69 +57,156 @@ func main() {
 	e.Use(slogecho.New(slog.Default()))
 	e.Use(middleware.Recover())
 
-	e.GET("/", htmlNodeHandler(func() ([]Node, error) {
-		return page(index)
+	users := &usersService{}
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			cc := &serviceContext{
+				Context: c,
+				users:   users,
+			}
+			return next(cc)
+		}
+	})
+
+	e.GET("/", htmlNodeHandler(func(c *serviceContext) (Node, error) {
+		isLoggedIn, err := c.users.IsLoggedInCookie(c.Cookie("auth"))
+		if err != nil {
+			return nil, err
+		}
+		if isLoggedIn {
+			return page(c, loggedIn)
+		} else {
+			return page(c, notLoggedIn)
+		}
 	}))
 
-	clicks := 0
-	e.POST("/clicks", htmlNodeHandler(func() ([]Node, error) {
-		clicks++
-		return clickResults(clicks)
-	}))
+	e.POST("/api/login", htmlNodeHandler(login))
 
 	addr := "127.0.0.1:8000"
 	slog.Info("listening", "addr", addr)
 	e.Logger.Fatal(e.Start(addr))
 }
 
-func index() ([]Node, error) {
-	return []Node{
-		H1(Text("Hello, World!")),
-		Button(
-			Attr("hx-post", "/clicks"),
-			Attr("hx-swap", "innerHTML"),
-			Attr("hx-target", "#clickResults"),
-			Text("Click Me"),
+func notLoggedIn(c *serviceContext) (Node, error) {
+	return Group([]Node{
+		FormEl(
+			Attr("hx-post", "/api/login"),
+			Attr("hx-swap", "none"),
+			Div(
+				Label(
+					For("username"),
+					Text("Username:"),
+				),
+				Input(
+					Name("username"),
+					Placeholder("Username"),
+					Type("text"),
+				),
+			),
+			Div(
+				Label(
+					For("password"),
+					Text("Password:"),
+				),
+				Input(
+					Name("password"),
+					Placeholder("Password"),
+					Type("password"),
+				),
+			),
+			Button(
+				Type("submit"),
+				Text("Log In"),
+			),
 		),
-		Div(ID("clickResults")),
-	}, nil
+		Div(ID("errorMessages")),
+	}), nil
 }
 
-func clickResults(clicks int) ([]Node, error) {
-	return []Node{
-		Div(Textf("Clicks: %d", clicks)),
-	}, nil
+func loggedIn(c *serviceContext) (Node, error) {
+	return Div(Text("TODO logged in page")), nil
 }
 
-func page(content func() ([]Node, error)) ([]Node, error) {
-	c, err := content()
+func login(c *serviceContext) (Node, error) {
+	var request struct {
+		Username string `form:"username"`
+		Password string `form:"password"`
+	}
+	if err := c.Bind(&request); err != nil {
+		return nil, err
+	}
+
+	slog.Debug("login", "request", request)
+
+	if len(request.Username) == 0 {
+		return errorMessage(c, "Username is required")
+	}
+
+	if len(request.Password) == 0 {
+		return errorMessage(c, "Password is required")
+	}
+
+	isValid, err := c.users.ValidateCredentials(request.Username, request.Password)
 	if err != nil {
 		return nil, err
 	}
-	return []Node{
-		HTML5(HTML5Props{
-			Head: []Node{
-				Script(
-					Src("https://unpkg.com/htmx.org@1.9.6"),
-					Text("htmx.logAll();"),
-				),
-			},
-			Body: c,
-		}),
-	}, nil
+	if !isValid {
+		return errorMessage(c, "Invalid credentials")
+	}
+
+	authCookie := new(http.Cookie)
+	authCookie.Name = "auth"
+	authCookie.Value = "TODO jwt here"
+	// TODO expire should match jwt
+	authCookie.Expires = time.Now().Add(60 * time.Second)
+	authCookie.Path = "/"
+	c.SetCookie(authCookie)
+
+	c.Response().Header().Add("hx-refresh", "true")
+	return loggedIn(c)
 }
 
-func htmlNodeHandler(f func() ([]Node, error)) echo.HandlerFunc {
+func errorMessage(c *serviceContext, msg string) (Node, error) {
+	return Div(
+		Attr("hx-swap-oob", "innerHTML:#errorMessages"),
+		Div(
+			Class("error"),
+			Text(msg),
+		),
+	), nil
+}
+
+func page(c *serviceContext, content func(c *serviceContext) (Node, error)) (Node, error) {
+	contentNode, err := content(c)
+	if err != nil {
+		return nil, err
+	}
+	return HTML5(HTML5Props{
+		Head: []Node{
+			StyleEl(Text(`
+				.error {
+					font-weight: bold;
+					color: red;
+				}
+			`)),
+			Script(
+				Src("https://unpkg.com/htmx.org@1.9.6"),
+				Text("htmx.logAll();"),
+			),
+		},
+		Body: []Node{contentNode},
+	}), nil
+}
+
+func htmlNodeHandler(f func(c *serviceContext) (Node, error)) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		nodes, err := f()
+		node, err := f(c.(*serviceContext))
 		if err != nil {
 			return err
 		}
 		var s strings.Builder
-		for _, node := range nodes {
-			if err := node.Render(&s); err != nil {
-				return err
-			}
+		if err := node.Render(&s); err != nil {
+			return err
 		}
 		return c.HTML(http.StatusOK, s.String())
 	}
