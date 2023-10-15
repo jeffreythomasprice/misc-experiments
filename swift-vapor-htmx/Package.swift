4c2429c20bcb2dcea712cc57a18f0b7cfc1e0677
Jeff Price <j.price@servicepower.com>
2023-10-15 06:06:25 -0400

    .

--- swift-vapor-htmx/Package.swift
+++ swift-vapor-htmx/Package.swift
@@ -2,31 +2,33 @@
 import PackageDescription
 
 let package = Package(
-    name: "experiment",
-    platforms: [
-       .macOS(.v13)
-    ],
-    dependencies: [
-        // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.83.1"),
-        // 🍃 An expressive, performant, and extensible templating language built for Swift.
-        .package(url: "https://github.com/vapor/leaf.git", from: "4.2.4"),
-    ],
-    targets: [
-        .executableTarget(
-            name: "App",
-            dependencies: [
-                .product(name: "Leaf", package: "leaf"),
-                .product(name: "Vapor", package: "vapor"),
-            ]
-        ),
-        .testTarget(name: "AppTests", dependencies: [
-            .target(name: "App"),
-            .product(name: "XCTVapor", package: "vapor"),
+	name: "experiment",
+	platforms: [
+		.macOS(.v13)
+	],
+	dependencies: [
+		.package(url: "https://github.com/vapor/vapor.git", from: "4.83.1"),
+		.package(url: "https://github.com/vapor/leaf.git", from: "4.2.4"),
+		.package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.14.1"),
+	],
+	targets: [
+		.executableTarget(
+			name: "App",
+			dependencies: [
+				.product(name: "Leaf", package: "leaf"),
+				.product(name: "Vapor", package: "vapor"),
+				.product(name: "SQLite", package: "SQLite.swift"),
+			]
+		),
+		.testTarget(
+			name: "AppTests",
+			dependencies: [
+				.target(name: "App"),
+				.product(name: "XCTVapor", package: "vapor"),
 
-            // Workaround for https://github.com/apple/swift-package-manager/issues/6940
-            .product(name: "Vapor", package: "vapor"),
-            .product(name: "Leaf", package: "leaf"),
-        ])
-    ]
+				// Workaround for https://github.com/apple/swift-package-manager/issues/6940
+				.product(name: "Vapor", package: "vapor"),
+				.product(name: "Leaf", package: "leaf"),
+			]),
+	]
 )
--- swift-vapor-htmx/Sources/App/entrypoint.swift
+++ swift-vapor-htmx/Sources/App/entrypoint.swift
@@ -1,71 +1,121 @@
 import Dispatch
 import Leaf
 import Logging
+import SQLite
 import Vapor
 
 extension Vapor.Application {
-  fileprivate static let baseExecutionQueue = DispatchQueue(label: "vapor.codes.entrypoint")
+	fileprivate static let baseExecutionQueue = DispatchQueue(label: "vapor.codes.entrypoint")
 
-  fileprivate func runFromAsyncMainEntrypoint() async throws {
-    try await withCheckedThrowingContinuation { continuation in
-      Vapor.Application.baseExecutionQueue.async { [self] in
-        do {
-          try self.run()
-          continuation.resume()
-        } catch {
-          continuation.resume(throwing: error)
-        }
-      }
-    }
-  }
+	fileprivate func runFromAsyncMainEntrypoint() async throws {
+		try await withCheckedThrowingContinuation { continuation in
+			Vapor.Application.baseExecutionQueue.async { [self] in
+				do {
+					try self.run()
+					continuation.resume()
+				} catch {
+					continuation.resume(throwing: error)
+				}
+			}
+		}
+	}
 }
 
 @main
 enum Entrypoint {
-  static func main() async throws {
-    var env = try Environment.detect()
-    try LoggingSystem.bootstrap(from: &env)
+	static func main() async throws {
+		LoggingSystem.bootstrap(console: Terminal(), level: .debug, metadata: [String: Logger.MetadataValue]())
 
-    let app = Application(env)
-    defer { app.shutdown() }
+		let env = try Environment.detect()
+		let app = Application(env)
+		defer { app.shutdown() }
 
-    do {
-      try await configure(app)
-    } catch {
-      app.logger.report(error: error)
-      throw error
-    }
-    try await app.runFromAsyncMainEntrypoint()
-  }
+		do {
+			try await configure(app)
+		} catch {
+			app.logger.report(error: error)
+			throw error
+		}
+		try await app.runFromAsyncMainEntrypoint()
+	}
 }
 
 public func configure(_ app: Application) async throws {
-  app.http.server.configuration.port = 8000
+	let log = app.logger
 
-  // uncomment to serve files from /Public folder
-  // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+	app.http.server.configuration.port = 8000
 
-  app.views.use(.leaf)
+	// uncomment to serve files from /Public folder
+	// app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-  try routes(app)
+	app.views.use(.leaf)
+
+	let db = try DBService(
+		log: log,
+		connection: try Connection(.uri("./db.sqlite"))
+	)
+
+	try routes(app: app, db: db)
 }
 
-func routes(_ app: Application) throws {
-  app.get { req async throws in
-    try await req.view.render("index", [String: String]())
-  }
+func routes(app: Application, db: DBService) throws {
+	let log = app.logger
 
-  var count = 0
-  let clickResults = { (req: Request) async throws -> View in
-    try await req.view.render("clickResults", ["count": count.description])
-  }
+	app.get { req async throws -> Vapor.Response in
+		return switch req.cookies["auth"]?.string {
+		// TODO actually check if auth is a valid jwt
+		case .some(_):
+			req.redirect(to: "/loggedIn")
+		case .none:
+			req.redirect(to: "/login")
+		}
+	}
 
-  app.get("click") { req async throws in
-    try await clickResults(req)
-  }
+	app.get("login") { req async throws -> Vapor.View in
+		try await req.view.render("login")
+	}
 
-  app.post("click") { req async throws -> View in
-    count += 1
-    return try await clickResults(req)
-  }
+	app.get("loggedIn") { req async throws -> Vapor.View in
+		try await req.view.render("loggedIn")
+	}
+
+	app.post("api", "login") { req async throws -> Vapor.Response in
+		struct Request: Content {
+			let username: String
+			let password: String
+		}
+		let reqBody = try req.content.decode(Request.self)
+		log.debug("login: \(reqBody)")
+
+		switch db.getUser(username: reqBody.username, password: reqBody.password)
+		{
+		case .success(.some(_)):
+			let res = Response()
+
+			var cookie = HTTPCookies.Value(string: "TODO jwt here")
+			// TODO use jwt expiration, in seconds
+			cookie.expires = Date.now.advanced(by: 60 * 5)
+			res.headers.setCookie = ["auth": cookie]
+
+			res.headers.add(name: "hx-location", value: "/loggedIn")
+
+			return res
+		// TODO use a struct for errors?
+		case .success(.none):
+
+			return try await req.view.render(
+				"loginError",
+				[
+					"message": "Invalid credentials"
+				]
+			).encodeResponse(for: req)
+		case .failure(let e):
+			return try await req.view.render(
+				"loginError",
+				[
+					"message": "Unexpected error: \(e)"
+				]
+			).encodeResponse(for: req)
+		}
+	}
 }
