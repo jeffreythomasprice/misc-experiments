81169fd43407cff39af439795a852722b438ac7e
Jeff Price <j.price@servicepower.com>
2023-11-12 18:32:54 -0500

    .

--- fsharp-giraffe-bolero/Client/Main.fs
+++ fsharp-giraffe-bolero/Client/Main.fs
@@ -8,59 +8,118 @@ open Elmish
 open Bolero
 open Bolero.Html
 
-type Counter() =
-    inherit ElmishComponent<int, int>()
+// TODO deduplicate
+[<CLIMutable>]
+type LoginRequest = { username: string; password: string }
 
-    override this.View model dispatch =
-        concat {
-            div { $"count: {model}" }
+// TODO deduplicate
+type LoginResponse = { username: string }
 
-            button {
-                on.click (fun _ -> dispatch (model + 1))
-                "Click Me"
+module LoginForm =
+    open System.Threading
+    type Model = { username: string; password: string }
+
+    type Message =
+        | SetUsername of string
+        | SetPassword of string
+        | Submit
+        | SubmitSuccess of LoginResponse
+        | SubmitError of exn
+
+    let init () : Model = { username = ""; password = "" }
+
+    let update (http: HttpClient) (message: Message) (model: Model) =
+        match message with
+        | SetUsername username -> { model with username = username }, Cmd.none
+        | SetPassword password -> { model with password = password }, Cmd.none
+        | Submit ->
+            printfn "TODO login, model = %A" model
+
+            let submit (request: LoginRequest) =
+                async {
+                    let! response = http.PostAsJsonAsync("/login", request) |> Async.AwaitTask
+
+                    return ()
+                }
+
+            let cmd =
+                Cmd.OfAsync.either
+                    (fun _ ->
+                        async {
+                            let request: LoginRequest =
+                                { username = model.username
+                                  password = model.password }
+
+                            let! response = http.PostAsJsonAsync("/login", request) |> Async.AwaitTask
+                            response.EnsureSuccessStatusCode() |> ignore
+
+                            let! responseBody = response.Content.ReadFromJsonAsync<LoginResponse>() |> Async.AwaitTask
+                            return responseBody
+                        })
+                    ()
+                    (fun result -> SubmitSuccess result)
+                    (fun e -> SubmitError e)
+
+            model, cmd
+        | SubmitSuccess response ->
+            printfn "TODO login success, %A" response
+            model, Cmd.none
+        | SubmitError e ->
+            printfn "TODO login failure, %A" e
+            model, Cmd.none
+
+    let view (model: Model) (dispatch: Message -> unit) =
+        form {
+            attr.id "login"
+
+            on.submit (fun _ -> dispatch Submit)
+
+            label {
+                attr.``for`` "username"
+                "Username:"
+            }
+
+            input {
+                attr.name "username"
+                attr.``type`` "text"
+                on.change (fun e -> dispatch (SetUsername(unbox e.Value)))
+            }
+
+            label {
+                attr.``for`` "password"
+                "Password:"
+            }
+
+            input {
+                attr.name "password"
+                attr.``type`` "password"
+                on.change (fun e -> dispatch (SetPassword(unbox e.Value)))
+            }
+
+            div { attr.empty () }
+
+            div {
+                button {
+                    attr.``type`` "submit"
+                    "Login"
+                }
             }
         }
 
-type Model = { counter: int }
+type Model = { loginForm: LoginForm.Model }
 
-let initModel = { counter = 0 }
+let initModel = { loginForm = LoginForm.init () }
 
-type Message =
-    // TODO no
-    | TestApi
-    | TestApiSuccess
-    | Error of exn
-    | SetCounter of int
+type Message = LoginForm of LoginForm.Message
 
 let update (http: HttpClient) message model =
     match message with
-    // TODO no
-    | TestApi ->
-        let testApi () =
-            task {
-                let! response = http.GetAsync("/test")
-                printfn "status code = %A" response.StatusCode
-                let! responseBody = response.Content.ReadAsStringAsync()
-                printfn "response body = %s" responseBody
-            }
-
-        let cmd = Cmd.OfTask.either testApi () (fun _ -> TestApiSuccess) Error
-        model, cmd
-    | TestApiSuccess ->
-        printfn "test api success"
-        model, Cmd.none
-    | Error e ->
-        printfn "error %A" e
-        model, Cmd.none
-    | SetCounter newCount ->
-        printfn "update counter to %A" newCount
-        { model with counter = newCount }, Cmd.none
+    | LoginForm message ->
+        let updated, cmd = LoginForm.update http message model.loginForm
+        { model with loginForm = updated }, (Cmd.map (fun msg -> LoginForm msg) cmd)
 
 let view model dispatch =
-    div {
-        ecomp<Counter, _, _> model.counter (fun newCount -> dispatch (SetCounter newCount)) { attr.empty () }
-        div { "Hello, World!" }
-    }
+    div { LoginForm.view model.loginForm (fun message -> dispatch (LoginForm message)) }
 
 type MyApp() =
     inherit ProgramComponent<Model, Message>()
@@ -71,4 +130,4 @@ type MyApp() =
     override this.Program =
         let update = update this.HttpClient
 
-        Program.mkProgram (fun _ -> initModel, Cmd.ofMsg TestApi) update view
+        Program.mkProgram (fun _ -> initModel, Cmd.none) update view
--- fsharp-giraffe-bolero/Server/Program.fs
+++ fsharp-giraffe-bolero/Server/Program.fs
@@ -10,13 +10,70 @@ open Microsoft.Extensions.Logging
 open Microsoft.Extensions.DependencyInjection
 open Giraffe
 open System.Reflection
+open System.Data.Common
+open Dapper
+open Microsoft.Data.Sqlite
+open Microsoft.AspNetCore.Http
 
-// TODO no
-let testHandler = setStatusCode 418 >=> text "I'm a teapot!"
+let initDb (db: DbConnection) =
+    task {
+        let! _ =
+            db.ExecuteAsync
+                @"CREATE TABLE IF NOT EXISTS users (
+                    username STRING NOT NULL UNIQUE PRIMARY KEY,
+                    password STRING NOT NULL
+                )"
 
-let webApp =
+        let! _ = db.ExecuteAsync @"INSERT OR IGNORE INTO users (username, password) VALUES (""admin"", ""admin"")"
+        ()
+    }
+
+type CredentialsCheck =
+    | Success
+    | BadCredentials
+
+type Db(db: DbConnection) =
+    member this.CheckUsernameAndPassword (username: string) (password: string) =
+        task {
+            let! count =
+                db.ExecuteScalarAsync<int>(
+                    "SELECT COUNT(*) FROM users WHERE username = @username AND password = @password",
+                    {| username = username
+                       password = password |}
+                )
+
+            return
+                match count with
+                | 1 -> Success
+                | _ -> BadCredentials
+        }
+
+// TODO deduplicate
+[<CLIMutable>]
+type LoginRequest = { username: string; password: string }
+
+// TODO deduplicate
+type LoginResponse = { username: string }
+
+let loginHandler (db: Db) : HttpHandler =
+    fun (next: HttpFunc) (ctx: HttpContext) ->
+        task {
+            let! request = ctx.BindJsonAsync<LoginRequest>()
+
+            let! result = db.CheckUsernameAndPassword request.username request.password
+
+            let responseBody: LoginResponse = { username = request.username }
+
+            return!
+                match result with
+                | CredentialsCheck.Success -> Successful.OK responseBody next ctx
+                // | BadCredentials -> Successful.OK (Failure "invalid credentials") next ctx
+                | BadCredentials -> RequestErrors.UNAUTHORIZED "schema" "realm" (Failure "invalid credentials") next ctx
+        }
+
+let webApp db =
     choose
-        [ choose [ GET >=> route "/test" >=> testHandler ]
+        [ choose [ POST >=> route "/login" >=> loginHandler db ]
           // TODO better 404 page
           setStatusCode 404 >=> text "Not Found" ]
 
@@ -28,7 +85,7 @@ let errorHandler (ex: Exception) (logger: ILogger) =
 let configureCors (builder: CorsPolicyBuilder) =
     builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader() |> ignore
 
-let configureApp =
+let configureApp db =
     fun (app: IApplicationBuilder) ->
         let env = app.ApplicationServices.GetService<IWebHostEnvironment>()
 
@@ -37,7 +94,7 @@ let configureApp =
          | false -> app.UseGiraffeErrorHandler(errorHandler).UseHttpsRedirection())
             .UseCors(configureCors)
             .UseStaticFiles()
-            .UseGiraffe(webApp)
+            .UseGiraffe(webApp db)
 
 let configureServices (services: IServiceCollection) =
     services.AddCors() |> ignore
@@ -51,6 +108,10 @@ let main args =
     let contentRoot = Directory.GetCurrentDirectory()
     let webRoot = Path.Combine(contentRoot, "WebRoot")
     let exeDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
+    let dbPath = Path.Combine(exeDir, "db.sqlite")
+
+    use db = new SqliteConnection $"Data Source={dbPath}"
+    initDb db |> Async.AwaitTask |> Async.RunSynchronously
 
     Host
         .CreateDefaultBuilder(args)
@@ -59,7 +120,7 @@ let main args =
                 .UseUrls("http://localhost:8001")
                 .UseContentRoot(contentRoot)
                 .UseWebRoot(webRoot)
-                .Configure(Action<IApplicationBuilder>(configureApp))
+                .Configure(Action<IApplicationBuilder>(configureApp (Db db)))
                 .ConfigureServices(configureServices)
                 .ConfigureLogging(configureLogging)
             |> ignore)
