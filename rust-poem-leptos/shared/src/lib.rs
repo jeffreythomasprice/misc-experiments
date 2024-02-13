fd08fb76147322c7ce13ddd8ca1f5f18d646dc5b
Jeffrey Price <jeffrey.thomas.price@gmail.com>
2024-02-13 05:48:47 -0500

    .

--- rust-poem-leptos/shared/src/lib.rs
+++ rust-poem-leptos/shared/src/lib.rs
@@ -4,7 +4,7 @@ use std::num::TryFromIntError;
 
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
-use web_time::{SystemTimeError, UNIX_EPOCH};
+use uuid::Uuid;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ClicksResponse {
@@ -13,7 +13,8 @@ pub struct ClicksResponse {
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ClientToServerChatMessage {
-    // TODO message id
+    #[serde(with = "uuid_format")]
+    pub id: Uuid,
     #[serde(with = "date_format")]
     pub timestamp: DateTime<Utc>,
     pub message: String,
@@ -22,6 +23,7 @@ pub struct ClientToServerChatMessage {
 impl ClientToServerChatMessage {
     pub fn new(msg: String) -> Result<Self, TimeError> {
         Ok(Self {
+            id: Uuid::new_v4(),
             timestamp: now_utc()?,
             message: msg,
         })
@@ -30,18 +32,20 @@ impl ClientToServerChatMessage {
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ServerToClientChatMessage {
-    // TODO message id
+    #[serde(with = "uuid_format")]
+    pub id: Uuid,
     #[serde(with = "date_format")]
     pub timestamp: DateTime<Utc>,
     #[serde(with = "date_format")]
     pub message_timestamp: DateTime<Utc>,
-    // TODO which sender
+    // TODO JEFF sender id
     pub message: String,
 }
 
 impl ServerToClientChatMessage {
     pub fn new(msg: &ClientToServerChatMessage) -> Result<Self, TimeError> {
         Ok(Self {
+            id: Uuid::new_v4(),
             timestamp: now_utc()?,
             message_timestamp: msg.timestamp,
             message: msg.message.clone(),
@@ -68,15 +72,36 @@ pub fn now_utc() -> Result<DateTime<Utc>, TimeError> {
     DateTime::<Utc>::from_timestamp_millis(result).ok_or(TimeError::DateTimeFromMillis)
 }
 
+mod uuid_format {
+    use std::str::FromStr;
+
+    use serde::{Deserialize, Deserializer, Serializer};
+    use uuid::Uuid;
+
+    pub fn serialize<S>(value: &Uuid, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(&value.to_string())
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Uuid, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        Uuid::from_str(&String::deserialize(deserializer)?).map_err(|e| serde::de::Error::custom(e))
+    }
+}
+
 mod date_format {
     use chrono::{DateTime, Utc};
     use serde::{Deserialize, Deserializer, Serializer};
 
-    pub fn serialize<S>(date: &DateTime<Utc>, serializer: S) -> Result<S::Ok, S::Error>
+    pub fn serialize<S>(value: &DateTime<Utc>, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
-        let s = date.to_rfc3339_opts(chrono::SecondsFormat::Millis, true);
+        let s = value.to_rfc3339_opts(chrono::SecondsFormat::Millis, true);
         serializer.serialize_str(&s)
     }
 
