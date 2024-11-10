e79b1154a0ef85729f40678debd81096ca83367f
Jeff Price <jeffrey.thomas.price@gmail.com>
2024-11-10 07:14:15 -0500

    .

---
+++ rust-kafka/src/kafka.rs
@@ -0,0 +1,322 @@
+use std::{collections::HashMap, env, fmt::Debug, time::Duration};
+
+use anyhow::{anyhow, Result};
+use clap::{command, Parser, Subcommand};
+use rdkafka::{
+    admin::{AdminClient, AdminOptions, NewTopic, TopicReplication},
+    client::DefaultClientContext,
+    config::RDKafkaLogLevel,
+    consumer::{Consumer, ConsumerContext, StreamConsumer},
+    message::{Header, Headers, OwnedHeaders},
+    producer::{FutureProducer, FutureRecord},
+    types::RDKafkaErrorCode,
+    util::get_rdkafka_version,
+    ClientConfig, ClientContext, Message,
+};
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use tokio::{
+    spawn,
+    sync::mpsc::{channel, Receiver},
+};
+use tracing::*;
+use tracing_subscriber::EnvFilter;
+use uuid::{ContextV7, Timestamp, Uuid};
+
+#[derive(Debug)]
+pub struct ConsumerConfig {
+    pub bootstrap_servers: String,
+    pub group_id: String,
+    pub topics: Vec<String>,
+}
+
+#[derive(Debug)]
+pub struct ProducerConfig<T> {
+    pub bootstrap_servers: String,
+    pub message: CustomMessage<T>,
+}
+
+// TODO rename me
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct CustomMessage<T> {
+    pub topic: String,
+    pub key: String,
+    pub headers: HashMap<String, String>,
+    pub payload: T,
+}
+
+struct KafkaContext;
+
+impl ClientContext for KafkaContext {
+    fn log(&self, level: RDKafkaLogLevel, fac: &str, log_message: &str) {
+        match level {
+            RDKafkaLogLevel::Emerg
+            | RDKafkaLogLevel::Alert
+            | RDKafkaLogLevel::Critical
+            | RDKafkaLogLevel::Error => error!(
+                "kafka log, level: {:?}, fac: {}, message: {}",
+                level, fac, log_message
+            ),
+            RDKafkaLogLevel::Warning => warn!(
+                "kafka log, level: {:?}, fac: {}, message: {}",
+                level, fac, log_message
+            ),
+            RDKafkaLogLevel::Notice | RDKafkaLogLevel::Info => info!(
+                "kafka log, level: {:?}, fac: {}, message: {}",
+                level, fac, log_message
+            ),
+            RDKafkaLogLevel::Debug => debug!(
+                "kafka log, level: {:?}, fac: {}, message: {}",
+                level, fac, log_message
+            ),
+        }
+    }
+
+    fn error(&self, error: rdkafka::error::KafkaError, reason: &str) {
+        error!("kafka error, err: {:?}, reason: {}", error, reason);
+    }
+}
+
+impl ConsumerContext for KafkaContext {
+    fn pre_rebalance<'a>(&self, rebalance: &rdkafka::consumer::Rebalance<'a>) {
+        info!("pre_rebalance: {:?}", rebalance);
+    }
+
+    fn post_rebalance<'a>(&self, rebalance: &rdkafka::consumer::Rebalance<'a>) {
+        info!("post_rebalance: {:?}", rebalance);
+    }
+
+    fn commit_callback(
+        &self,
+        result: rdkafka::error::KafkaResult<()>,
+        offsets: &rdkafka::TopicPartitionList,
+    ) {
+        info!("commit_callbackcommit_: {:?}, {:?}", result, offsets);
+    }
+}
+
+pub async fn consume<T>(config: ConsumerConfig) -> Result<Receiver<CustomMessage<T>>>
+where
+    T: DeserializeOwned + Debug + Send + 'static,
+{
+    if config.topics.len() == 0 {
+        Err(anyhow!("must provide at least one topic"))?;
+    }
+
+    create_topics(&config.bootstrap_servers, &config.topics).await?;
+
+    let context = KafkaContext;
+
+    let consumer: StreamConsumer<KafkaContext> = client_config(&config.bootstrap_servers)
+        .set("group.id", config.group_id)
+        .set("enable.partition.eof", "false")
+        .set("session.timeout.ms", "6000")
+        .set("enable.auto.commit", "true")
+        .create_with_context(context)
+        .map_err(|e| anyhow!("error creating consumer: {e:?}"))?;
+
+    consumer.subscribe(
+        config
+            .topics
+            .iter()
+            .map(|s| s.as_str())
+            .collect::<Vec<_>>()
+            .as_slice(),
+    )?;
+    trace!("consumer subscribed to topics: {:?}", config.topics);
+
+    let (sender, receiver) = channel(1);
+
+    spawn(async move {
+        loop {
+            let message = match consumer.recv().await {
+                Ok(message) => message,
+                Err(e) => {
+                    error!("error receiving message from kafka: {e:?}");
+                    continue;
+                }
+            };
+
+            let key = match message.key().map(|s| std::str::from_utf8(s)) {
+                Some(Ok(key)) => key.to_owned(),
+                Some(Err(e)) => {
+                    warn!(
+                        "message key isn't utf8, topic: {}, error: {:?}",
+                        message.topic(),
+                        e
+                    );
+                    continue;
+                }
+                None => {
+                    warn!("message missing key, topic: {}", message.topic());
+                    continue;
+                }
+            };
+
+            let headers = match message.headers() {
+                Some(headers) => {
+                    let mut results = HashMap::new();
+                    results.reserve(headers.count());
+                    for header in headers.iter() {
+                        let value = match header.value.map(|value| std::str::from_utf8(value)) {
+                            Some(Ok(value)) => value,
+                            Some(Err(e)) => {
+                                warn!("header value failed to parse as utf8, topic: {}, key: {}, header: {}, error: {:?}", message.topic(), key, header.key, e);
+                                continue;
+                            }
+                            None => {
+                                warn!(
+                                    "header value missing, topic: {}, key: {}, header: {}",
+                                    message.topic(),
+                                    key,
+                                    header.key
+                                );
+                                continue;
+                            }
+                        };
+                        results.insert(header.key.to_owned(), value.to_owned());
+                    }
+                    results
+                }
+                None => HashMap::new(),
+            };
+
+            let payload = match message.payload().map(|s| std::str::from_utf8(s)) {
+                Some(Ok(payload)) => payload,
+                Some(Err(e)) => {
+                    warn!(
+                        "message payload isn't utf8, topic: {}, key: {}, error: {:?}",
+                        message.topic(),
+                        key,
+                        e
+                    );
+                    continue;
+                }
+                None => {
+                    warn!(
+                        "message missing payload, topic: {}, key: {}",
+                        message.topic(),
+                        key
+                    );
+                    continue;
+                }
+            };
+
+            let payload = match serde_json::from_str::<T>(payload) {
+                Ok(payload) => payload,
+                Err(e) => {
+                    warn!("error deserializing message payload as expected type, topic: {}, key: {}, payload: {}, error: {:?}", message.topic(), key, payload, e);
+                    continue;
+                }
+            };
+
+            let parsed_message = CustomMessage {
+                topic: message.topic().to_owned(),
+                key,
+                headers,
+                payload,
+            };
+            trace!("received message: {:?}", parsed_message);
+            let key = parsed_message.key.clone();
+            if let Err(e) = sender.send(parsed_message).await {
+                error!(
+                    "error sending parsed message to channel, topic: {}, key: {}, error: {:?}",
+                    message.topic(),
+                    key,
+                    e
+                );
+            }
+
+            if let Err(e) = consumer.commit_message(&message, rdkafka::consumer::CommitMode::Async)
+            {
+                error!("error committing message to kafka: {e:?}");
+            }
+        }
+    });
+
+    Ok(receiver)
+}
+
+pub async fn produce<T>(config: ProducerConfig<T>) -> Result<()>
+where
+    T: Serialize + Debug,
+{
+    info!("sending message: {:?}", config);
+
+    create_topics(
+        &config.bootstrap_servers,
+        &vec![config.message.topic.clone()],
+    )
+    .await?;
+
+    // TODO make a reusable producer handle we can send messages to via a channel
+    let producer: FutureProducer = client_config(&config.bootstrap_servers)
+        .create()
+        .map_err(|e| anyhow!("error creating producer: {e:?}"))?;
+
+    let mut headers = OwnedHeaders::new();
+    for (key, value) in config.message.headers.iter() {
+        headers = headers.insert(Header {
+            key: &key,
+            value: Some(&value),
+        });
+    }
+
+    let payload = serde_json::to_string(&config.message.payload)?;
+
+    let result = producer
+        .send(
+            FutureRecord::to(&config.message.topic)
+                .payload(&payload)
+                .key(config.message.key.as_bytes())
+                .headers(headers),
+            Duration::from_secs(0),
+        )
+        .await
+        .map_err(|e| anyhow!("error sending message: {e:?}"))?;
+    debug!("message sent, message: {:?}, result: {:?}", config, result);
+
+    Ok(())
+}
+
+async fn create_topics(bootstrap_servers: &str, topics: &Vec<String>) -> Result<()> {
+    info!("creating topics: {:?}", topics);
+
+    let client: AdminClient<DefaultClientContext> = client_config(bootstrap_servers).create()?;
+
+    let topics = topics
+        .iter()
+        .map(|topic| NewTopic {
+            name: topic,
+            num_partitions: 3,
+            replication: TopicReplication::Fixed(1),
+            config: vec![],
+        })
+        .collect::<Vec<_>>();
+    debug!("topic configs: {:?}", topics);
+
+    let results = client
+        .create_topics(topics.iter().collect::<Vec<_>>(), &AdminOptions::default())
+        .await
+        .map_err(|e| anyhow!("error creating topics: {:?}, error: {:?}", topics, e))?;
+    for result in results {
+        match result {
+            Ok(topic) => trace!("topic created: {topic}"),
+            Err((topic, RDKafkaErrorCode::TopicAlreadyExists)) => {
+                trace!("topic already exists: {topic}")
+            }
+            Err((topic, e)) => Err(anyhow!("error creating topic {}, {:?}", topic, e))?,
+        };
+    }
+
+    Ok(())
+}
+
+fn client_config(bootstrap_servers: &str) -> ClientConfig {
+    // https://github.com/confluentinc/librdkafka/blob/master/CONFIGURATION.md
+    let mut result = ClientConfig::new();
+    result
+        .set("security.protocol", "plaintext")
+        .set("bootstrap.servers", bootstrap_servers)
+        .set_log_level(RDKafkaLogLevel::Debug);
+    result
+}
--- rust-kafka/src/main.rs
+++ rust-kafka/src/main.rs
@@ -1,18 +1,20 @@
-use std::{env, time::Duration};
+mod kafka;
+
+use std::{
+    env,
+    time::{Duration, SystemTime},
+};
 
 use anyhow::{anyhow, Result};
 use clap::{command, Parser, Subcommand};
+use kafka::{consume, produce, ConsumerConfig, CustomMessage, ProducerConfig};
 use rdkafka::{
-    admin::{AdminClient, AdminOptions, NewTopic, TopicReplication},
-    client::DefaultClientContext,
-    config::RDKafkaLogLevel,
-    consumer::{Consumer, ConsumerContext, StreamConsumer},
+    consumer::{Consumer, StreamConsumer},
     message::{Header, Headers, OwnedHeaders},
     producer::{FutureProducer, FutureRecord},
-    types::RDKafkaErrorCode,
     util::get_rdkafka_version,
-    ClientConfig, ClientContext, Message,
 };
+use serde::{Deserialize, Serialize};
 use tracing::*;
 use tracing_subscriber::EnvFilter;
 use uuid::{ContextV7, Timestamp, Uuid};
@@ -42,66 +44,9 @@ enum Commands {
     },
 }
 
-struct ConsumerConfig {
-    bootstrap_servers: String,
-    group_id: String,
-    topics: Vec<String>,
-}
-
-struct ProducerConfig {
-    bootstrap_servers: String,
-    topic: String,
-    payload: String,
-}
-
-struct KafkaContext;
-
-impl ClientContext for KafkaContext {
-    fn log(&self, level: RDKafkaLogLevel, fac: &str, log_message: &str) {
-        match level {
-            RDKafkaLogLevel::Emerg
-            | RDKafkaLogLevel::Alert
-            | RDKafkaLogLevel::Critical
-            | RDKafkaLogLevel::Error => error!(
-                "kafka log, level: {:?}, fac: {}, message: {}",
-                level, fac, log_message
-            ),
-            RDKafkaLogLevel::Warning => warn!(
-                "kafka log, level: {:?}, fac: {}, message: {}",
-                level, fac, log_message
-            ),
-            RDKafkaLogLevel::Notice | RDKafkaLogLevel::Info => info!(
-                "kafka log, level: {:?}, fac: {}, message: {}",
-                level, fac, log_message
-            ),
-            RDKafkaLogLevel::Debug => debug!(
-                "kafka log, level: {:?}, fac: {}, message: {}",
-                level, fac, log_message
-            ),
-        }
-    }
-
-    fn error(&self, error: rdkafka::error::KafkaError, reason: &str) {
-        error!("kafka error, err: {:?}, reason: {}", error, reason);
-    }
-}
-
-impl ConsumerContext for KafkaContext {
-    fn pre_rebalance<'a>(&self, rebalance: &rdkafka::consumer::Rebalance<'a>) {
-        info!("pre_rebalance: {:?}", rebalance);
-    }
-
-    fn post_rebalance<'a>(&self, rebalance: &rdkafka::consumer::Rebalance<'a>) {
-        info!("post_rebalance: {:?}", rebalance);
-    }
-
-    fn commit_callback(
-        &self,
-        result: rdkafka::error::KafkaResult<()>,
-        offsets: &rdkafka::TopicPartitionList,
-    ) {
-        info!("commit_callbackcommit_: {:?}, {:?}", result, offsets);
-    }
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Message {
+    message: String,
 }
 
 #[tokio::main]
@@ -121,161 +66,34 @@ async fn main() -> Result<()> {
     let cli = Cli::parse();
     match cli.command {
         Commands::Consumer { topics } => {
-            consumer(ConsumerConfig {
+            let mut receiver = consume::<Message>(ConsumerConfig {
                 bootstrap_servers: cli.bootstrap_servers,
                 group_id: "group id".to_owned(),
                 topics,
             })
-            .await
+            .await?;
+            while let Some(message) = receiver.recv().await {
+                info!("received message: {:?}", message);
+            }
+            Ok(())
         }
+
         Commands::Producer { topic, message } => {
-            producer(ProducerConfig {
+            produce(ProducerConfig {
                 bootstrap_servers: cli.bootstrap_servers,
-                topic,
-                payload: message,
+                message: CustomMessage {
+                    topic,
+                    key: Uuid::new_v7(Timestamp::now(ContextV7::new())).to_string(),
+                    headers: [(
+                        "example header".to_owned(),
+                        "example header value".to_owned(),
+                    )]
+                    .into_iter()
+                    .collect(),
+                    payload: Message { message },
+                },
             })
             .await
         }
     }
-}
-
-fn client_config(bootstrap_servers: &str) -> ClientConfig {
-    // https://github.com/confluentinc/librdkafka/blob/master/CONFIGURATION.md
-    let mut result = ClientConfig::new();
-    result
-        .set("security.protocol", "plaintext")
-        .set("bootstrap.servers", bootstrap_servers)
-        .set_log_level(RDKafkaLogLevel::Debug);
-    result
-}
-
-async fn create_topics(bootstrap_servers: &str, topics: &Vec<String>) -> Result<()> {
-    info!("creating topics: {:?}", topics);
-
-    let client: AdminClient<DefaultClientContext> = client_config(bootstrap_servers).create()?;
-
-    let topics = topics
-        .iter()
-        .map(|topic| NewTopic {
-            name: topic,
-            num_partitions: 3,
-            replication: TopicReplication::Fixed(1),
-            config: vec![],
-        })
-        .collect::<Vec<_>>();
-    debug!("topic configs: {:?}", topics);
-
-    let results = client
-        .create_topics(topics.iter().collect::<Vec<_>>(), &AdminOptions::default())
-        .await
-        .map_err(|e| anyhow!("error creating topics: {:?}, error: {:?}", topics, e))?;
-    for result in results {
-        match result {
-            Ok(topic) => trace!("topic created: {topic}"),
-            Err((topic, RDKafkaErrorCode::TopicAlreadyExists)) => {
-                trace!("topic already exists: {topic}")
-            }
-            Err((topic, e)) => Err(anyhow!("error creating topic {}, {:?}", topic, e))?,
-        };
-    }
-
-    Ok(())
-}
-
-async fn consumer(config: ConsumerConfig) -> Result<()> {
-    if config.topics.len() == 0 {
-        Err(anyhow!("must provide at least one topic"))?;
-    }
-
-    create_topics(&config.bootstrap_servers, &config.topics).await?;
-
-    let context = KafkaContext;
-
-    let consumer: StreamConsumer<KafkaContext> = client_config(&config.bootstrap_servers)
-        .set("group.id", config.group_id)
-        .set("enable.partition.eof", "false")
-        .set("session.timeout.ms", "6000")
-        .set("enable.auto.commit", "true")
-        .create_with_context(context)
-        .map_err(|e| anyhow!("error creating consumer: {e:?}"))?;
-
-    consumer.subscribe(
-        config
-            .topics
-            .iter()
-            .map(|s| s.as_str())
-            .collect::<Vec<_>>()
-            .as_slice(),
-    )?;
-    trace!("consumer subscribed to topics: {:?}", config.topics);
-
-    loop {
-        let message = consumer
-            .recv()
-            .await
-            .map_err(|e| anyhow!("error receiving message from kafka: {e:?}"))?;
-
-        let key = message.key().map(|s| std::str::from_utf8(s));
-        let headers = message.headers().map(|headers| {
-            headers
-                .iter()
-                .map(|header| {
-                    (
-                        header.key,
-                        header.value.map(|value| std::str::from_utf8(value)),
-                    )
-                })
-                .collect::<Vec<_>>()
-        });
-        let payload = message.payload().map(|s| std::str::from_utf8(s));
-        info!(
-            "received message, key {:?}, headers: {:?}, payload: {:?}",
-            key, headers, payload
-        );
-        consumer
-            .commit_message(&message, rdkafka::consumer::CommitMode::Async)
-            .map_err(|e| anyhow!("error committing message to kafka: {e:?}"))?;
-    }
-}
-
-async fn producer(config: ProducerConfig) -> Result<()> {
-    let producer: FutureProducer = client_config(&config.bootstrap_servers)
-        .create()
-        .map_err(|e| anyhow!("error creating producer: {e:?}"))?;
-
-    let uuid_context = ContextV7::new();
-    let key = Uuid::new_v7(Timestamp::now(uuid_context));
-
-    let headers = OwnedHeaders::new().insert(Header {
-        key: "example header key",
-        value: Some("example header value"),
-    });
-
-    let printable_headers = headers
-        .iter()
-        .map(|header| {
-            (
-                header.key,
-                header.value.map(|value| std::str::from_utf8(value)),
-            )
-        })
-        .collect::<Vec<_>>();
-    info!(
-        "sending message, key: {:?}, headers: {:?}, payload: {:?}",
-        key, printable_headers, config.payload
-    );
-
-    let result = producer
-        .send(
-            FutureRecord::to(&config.topic)
-                .payload(&config.payload)
-                .key(key.as_bytes())
-                .headers(headers),
-            Duration::from_secs(0),
-        )
-        .await
-        .map_err(|e| anyhow!("error sending message: {e:?}"))?;
-    debug!("message sent, key: {:?}, result: {:?}", key, result);
-
-    Ok(())
 }
