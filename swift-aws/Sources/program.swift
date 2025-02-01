61f0f4dd692e0e0b79144e07daa89896e3dabd2f
Jeff Price <jeffrey.thomas.price@gmail.com>
2025-01-31 19:59:12 -0500

    .

--- swift-aws/Sources/program.swift
+++ swift-aws/Sources/program.swift
@@ -12,6 +12,7 @@ struct Main: AsyncParsableCommand {
         subcommands: [
             Env.self,
             ListClusters.self,
+            DescribeCluster.self,
         ]
     )
 }
@@ -39,6 +40,34 @@ extension Main {
 
         mutating func run() async {
             await Program().listClusters(profile: options.profile)
+        }
+    }
+
+    struct DescribeCluster: AsyncParsableCommand {
+        static let configuration = CommandConfiguration(abstract: "Describe an ECS cluster")
+
+        @OptionGroup var options: Options
+
+        @Option(name: .customLong("cluster-arn"), help: "Look for cluster by ARN")
+        var clusterArn: String?
+
+        @Option(name: .customLong("cluster-name"), help: "Look for cluster by name")
+        var clusterName: String?
+
+        func validate() throws {
+            if (clusterArn == nil && clusterName == nil) || (clusterArn != nil && clusterName != nil) {
+                throw ValidationError("Must provide exactly one of cluster ARN or name")
+            }
+        }
+
+        mutating func run() async throws {
+            if let clusterArn = self.clusterArn {
+                await Program().describeCluster(profile: options.profile, clusterArn: clusterArn)
+            } else if let clusterName = self.clusterName {
+                await Program().describeCluster(profile: options.profile, clusterName: clusterName)
+            } else {
+                throw ValidationError("should be impossible")
+            }
         }
     }
 }
@@ -82,18 +111,98 @@ class Program {
     }
 
     func listClusters(profile: String) async {
+        await doWithProfile(profile: profile) { client in
+            let ecs = ECS(client: client)
+            for result in try await Paging({ nextToken async throws in
+                let results = try await ecs.listClusters(ECS.ListClustersRequest(nextToken: nextToken))
+                return (results.nextToken, results.clusterArns)
+            }).collect().sorted() {
+                print("\(result)")
+            }
+        }
+    }
+
+    func describeCluster(profile: String, clusterName: String) async {
+        await doWithProfile(profile: profile) { client in
+            let ecs = ECS(client: client)
+
+            let results = try await Paging({ nextToken async throws in
+                let results = try await ecs.listClusters(ECS.ListClustersRequest(nextToken: nextToken))
+                return (results.nextToken, results.clusterArns)
+            }).filter { clusterArn in
+                clusterArn.hasSuffix("/\(clusterName)")
+            }.collect()
+
+            if results.count > 1 {
+                print("multiple clusters found that match name")
+                for r in results {
+                    print("\(r)")
+                }
+                exit(1)
+            }
+
+            if results.count == 0 {
+                print("no clusters found that match name")
+                exit(1)
+            }
+
+            await describeCluster(profile: profile, clusterArn: results[0])
+        }
+    }
+
+    func describeCluster(profile: String, clusterArn: String) async {
+        await doWithProfile(profile: profile) { client in
+            let ecs = ECS(client: client)
+
+            let serviceArns = try await Paging({ nextToken async throws in
+                let result = try await ecs.listServices(ECS.ListServicesRequest(cluster: clusterArn, nextToken: nextToken))
+                return (result.nextToken, result.serviceArns)
+            }).collect().sorted()
+
+            let services = try await serviceArns.concurrentMap { serviceArn async throws in
+                let result = try await ecs.describeServices(ECS.DescribeServicesRequest(cluster: clusterArn, services: [serviceArn]))
+                if let failures = result.failures {
+                    if failures.count > 0 {
+                        print("failed to find service \(serviceArn): \(failures)")
+                        exit(1)
+                    }
+                }
+                guard let service = result.services?.first else {
+                    print("failed to find service \(serviceArn)")
+                    exit(1)
+                }
+                return service
+            }
+
+            let taskDefinitionArns = services.compactMap { service in
+                service.taskDefinition
+            }.uniqued()
+
+            let taskDefinitions = try await taskDefinitionArns.concurrentMap { taskDefinitionArn async throws in
+                try await ecs.describeTaskDefinition(ECS.DescribeTaskDefinitionRequest(taskDefinition: taskDefinitionArn)).taskDefinition
+            }.compacted()
+
+            for service in services {
+                print(
+                    "\(service.serviceName ?? "<N/A>"), task count \(service.runningCount?.description ?? "<N/A>")/\(service.desiredCount?.description ?? "<N/A>"), task definition \(service.taskDefinition ?? "<N/A>")"
+                )
+                if let taskDefinition = taskDefinitions.first { taskDefinition in
+                    taskDefinition.taskDefinitionArn == service.taskDefinition
+                } {
+                    for containerDefinition in taskDefinition.containerDefinitions ?? [] {
+                        print("    \(containerDefinition.name ?? "N/A") \(containerDefinition.image ?? "N/A")")
+                    }
+                }
+            }
+            print()
+        }
+    }
+
+    private func doWithProfile(profile: String, _ f: (AWSClient) async throws -> Void) async {
         await orExit {
             let credentials = try await getCredentials(profile: profile)
             try await doWithClient(client: credentials.createAWSClient()) { client in
-                let ecs = ECS(client: client)
-                for result in try await paging(
-                    request: ECS.ListClustersRequest(),
-                    getNextPage: { request in
-                        try await ecs.listClusters(request)
-                    }
-                ).sorted() {
-                    print("\(result)")
-                }
+                try await f(client)
             }
         }
     }
@@ -116,59 +225,5 @@ class Program {
             logger.error("fatal: \(error)")
             exit(1)
         }
-    }
-}
-
-protocol WithNextToken {
-    func withNextToken(nextToken: String) -> Self
-}
-
-protocol HasNextToken {
-    var nextToken: String? { get }
-}
-
-protocol HasDataPage<T> {
-    associatedtype T
-
-    var data: [T]? { get }
-}
-
-func paging<DataType, RequestType: WithNextToken, ResponseType: HasNextToken & HasDataPage<DataType>>(
-    request: RequestType,
-    getNextPage: (RequestType) async throws -> ResponseType
-) async throws -> [DataType] {
-    var results: [DataType] = []
-    var nextToken: String? = nil
-    while true {
-        let nextRequest =
-            if let nextToken = nextToken {
-                request.withNextToken(nextToken: nextToken)
-            } else {
-                request
-            }
-        let result = try await getNextPage(
-            nextRequest)
-        if let data = result.data {
-            results.append(contentsOf: data)
-        }
-        nextToken = result.nextToken
-        if nextToken == nil {
-            break
-        }
-    }
-    return results
-}
-
-extension ECS.ListClustersRequest: WithNextToken {
-    func withNextToken(nextToken: String) -> SotoECS.ECS.ListClustersRequest {
-        ECS.ListClustersRequest(maxResults: self.maxResults, nextToken: nextToken)
-    }
-}
-
-extension ECS.ListClustersResponse: HasNextToken & HasDataPage {
-    typealias T = String
-
-    var data: [String]? {
-        self.clusterArns
     }
 }
