f5ba716088e6ef793fd94665bb2063b93796ff65
Jeff Price <jeffrey.thomas.price@gmail.com>
2025-04-12 15:28:18 -0400

    .

--- rust-pixels-winit/src/main.rs
+++ rust-pixels-winit/src/main.rs
@@ -12,6 +12,8 @@ use winit::{
     window::{Window, WindowAttributes},
 };
 
+const DESIRED_SIZE: LogicalSize<u32> = LogicalSize::new(1024, 768);
+
 struct WindowState {
     window: Arc<Window>,
     pixels: Pixels<'static>,
@@ -19,7 +21,7 @@ struct WindowState {
 
 impl WindowState {
     pub fn new(event_loop: &ActiveEventLoop, display_size: LogicalSize<u32>) -> Result<Self> {
-        let window_size = PhysicalSize::new(1024, 768);
+        let window_size = PhysicalSize::new(DESIRED_SIZE.width, DESIRED_SIZE.height);
         let mut window_attributes = WindowAttributes::default()
             .with_title("Experiment")
             .with_inner_size(window_size);
@@ -27,8 +29,8 @@ impl WindowState {
             let monitor_position = monitor.position();
             let monitor_size = monitor.size();
             window_attributes = window_attributes.with_position(PhysicalPosition::new(
-                monitor_position.x + ((monitor_size.width as i32) - window_size.width) / 2,
-                monitor_position.y + ((monitor_size.height as i32) - window_size.height) / 2,
+                monitor_position.x + ((monitor_size.width - window_size.width) / 2) as i32,
+                monitor_position.y + ((monitor_size.height - window_size.height) / 2) as i32,
             ));
         }
         let window = Arc::new(event_loop.create_window(window_attributes)?);
@@ -57,7 +59,10 @@ impl App {
 impl ApplicationHandler for App {
     fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
         if self.window_state.is_none() {
-            match WindowState::new(event_loop, LogicalSize::new(1024, 768)) {
+            match WindowState::new(
+                event_loop,
+                LogicalSize::new(DESIRED_SIZE.width, DESIRED_SIZE.height),
+            ) {
                 Ok(window_state) => {
                     self.window_state.replace(window_state);
                 }
@@ -106,20 +111,15 @@ impl ApplicationHandler for App {
 
             winit::event::WindowEvent::RedrawRequested => {
                 if let Some(window_state) = &mut self.window_state {
-                    let width = window_state.pixels.texture().width();
-                    let height = window_state.pixels.texture().height();
+                    let width = window_state.pixels.texture().width() as usize;
+                    let height = window_state.pixels.texture().height() as usize;
                     let pixels = window_state.pixels.frame_mut();
-                    for y in 0..height {
+                    for (i, pixel) in pixels.chunks_exact_mut(4).enumerate() {
+                        let x = i % width;
+                        let y = i / width;
+                        let a = ((x as f64) / (width as f64) * 255.0) as u8;
                         let b = ((y as f64) / (height as f64) * 255.0) as u8;
-                        for x in 0..width {
-                            let a = ((x as f64) / (width as f64) * 255.0) as u8;
-                            let i = (4 * (x + y * width)) as usize;
-                            let pixel = &mut pixels[i..(i + 4)];
-                            pixel[0] = a;
-                            pixel[1] = b;
-                            pixel[2] = a;
-                            pixel[3] = 255;
-                        }
+                        pixel.copy_from_slice(&[a, b, a, 255]);
                     }
 
                     if let Err(e) = window_state.pixels.render() {
