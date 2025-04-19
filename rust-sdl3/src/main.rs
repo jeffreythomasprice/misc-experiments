6f2bcc955cfe192162f2d20212577d0be1b4330a
Jeff Price <jeffrey.thomas.price@gmail.com>
2025-04-19 12:49:03 -0400

    .

--- rust-sdl3/src/main.rs
+++ rust-sdl3/src/main.rs
@@ -1,7 +1,19 @@
-use std::time::Duration;
+mod fps;
+
+use std::time::{Duration, SystemTime};
 
 use color_eyre::eyre::{Result, eyre};
-use sdl3::{event::Event, keyboard::Keycode, pixels::Color, rect::Point};
+use fps::FPSCounter;
+use sdl3::{
+    event::Event,
+    iostream::IOStream,
+    keyboard::Keycode,
+    pixels::Color,
+    rect::Point,
+    render::{Canvas, FRect, RenderTarget, TextureCreator},
+    ttf::Font,
+    video::WindowContext,
+};
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -16,26 +28,42 @@ async fn main() -> Result<()> {
         .init();
 
     let sdl_context = sdl3::init()?;
+    let sdl_ttf_context = sdl3::ttf::init()?;
+
     let video_subsystem = sdl_context.video()?;
 
     let window = video_subsystem
         .window("Experiment", 1024, 768)
         .position_centered()
         .resizable()
+        .opengl()
         .build()?;
 
     let mut canvas = window.into_canvas();
     canvas.set_logical_size(
-        1024,
-        768,
+        640,
+        480,
         sdl3::sys::render::SDL_RendererLogicalPresentation::LETTERBOX,
+    )?;
+    let texture_creator = canvas.texture_creator();
+
+    let font = sdl_ttf_context.load_font_from_iostream(
+        IOStream::from_bytes(include_bytes!(
+            "../assets/calibri-font-family/calibri-regular.ttf"
+        ))?,
+        24.0,
     )?;
 
     let mut event_pump = sdl_context.event_pump()?;
 
-    const DESIRED_FPS: u32 = 30;
-    let sleep_time = Duration::from_secs_f64(1.0 / (DESIRED_FPS as f64));
+    const DESIRED_FPS: u32 = 60;
+    let desired_duration_per_frame = Duration::from_secs_f64(1.0 / (DESIRED_FPS as f64));
+    let mut fps = FPSCounter::new();
+    let mut last_frame_start = None;
+
     'running: loop {
+        let start = SystemTime::now();
+
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit { .. }
@@ -48,18 +76,87 @@ async fn main() -> Result<()> {
         }
 
         let (width, height, _) = canvas.logical_size();
-        for y in 0..height {
-            let b = ((y as f64) / (height as f64) * 255.0) as u8;
-            for x in 0..width {
-                let a = ((x as f64) / (width as f64) * 255.0) as u8;
-                canvas.set_draw_color(Color::RGB(a, b, a));
-                canvas.draw_point(Point::new(x as i32, y as i32))?;
-            }
-        }
+        // for y in 0..height {
+        //     let b = ((y as f64) / (height as f64) * 255.0) as u8;
+        //     for x in 0..width {
+        //         let a = ((x as f64) / (width as f64) * 255.0) as u8;
+        //         canvas.set_draw_color(Color::RGB(a, b, a));
+        //         canvas.draw_point(Point::new(x as i32, y as i32))?;
+        //     }
+        // }
+        canvas.set_draw_color(Color::RGB(64, 64, 64));
+        canvas.clear();
+        draw_string(
+            &mut canvas,
+            &texture_creator,
+            &font,
+            &format!("FPS: {}", fps.fps_pretty()),
+            Color::RGB(255, 255, 255),
+            FRect::new(0.0, 0.0, width as f32, height as f32),
+            HorizontalAlignment::Left,
+            VerticalAlignment::Top,
+        )?;
         canvas.present();
 
-        ::std::thread::sleep(sleep_time);
+        if let Some(last_frame_start) = last_frame_start {
+            let duration_between_frame_starts = start.duration_since(last_frame_start)?;
+            fps.tick(duration_between_frame_starts);
+        }
+        last_frame_start = Some(start);
+
+        let end = SystemTime::now();
+        let duration_this_frame = end.duration_since(start)?;
+        if desired_duration_per_frame > duration_this_frame {
+            ::std::thread::sleep(desired_duration_per_frame - duration_this_frame);
+        }
     }
 
+    Ok(())
+}
+
+enum HorizontalAlignment {
+    Left,
+    Center,
+    Right,
+}
+
+enum VerticalAlignment {
+    Top,
+    Center,
+    Bottom,
+}
+
+fn draw_string<T: RenderTarget>(
+    canvas: &mut Canvas<T>,
+    texture_creator: &TextureCreator<WindowContext>,
+    font: &Font,
+    s: &str,
+    color: Color,
+    dst: FRect,
+    halign: HorizontalAlignment,
+    valign: VerticalAlignment,
+) -> Result<()> {
+    let surface = font.render(s).blended(color)?;
+    let texture = texture_creator.create_texture_from_surface(surface)?;
+    let x = match halign {
+        HorizontalAlignment::Left => dst.x,
+        HorizontalAlignment::Center => dst.x + (dst.w - texture.width() as f32) * 0.5,
+        HorizontalAlignment::Right => dst.x + dst.w - texture.width() as f32,
+    };
+    let y = match valign {
+        VerticalAlignment::Top => dst.y,
+        VerticalAlignment::Center => dst.y + (dst.h - texture.height() as f32) * 0.5,
+        VerticalAlignment::Bottom => dst.y + dst.h - texture.height() as f32,
+    };
+    canvas.copy(
+        &texture,
+        None,
+        Some(FRect::new(
+            x,
+            y,
+            texture.width() as f32,
+            texture.height() as f32,
+        )),
+    )?;
     Ok(())
 }
