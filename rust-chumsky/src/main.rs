334c94e62be15319aa5edd998ab5a165df900dea
Jeff Price <jeffrey.thomas.price@gmail.com>
2025-07-05 10:07:08 -0400

    .

--- rust-chumsky/src/main.rs
+++ rust-chumsky/src/main.rs
@@ -97,6 +97,7 @@ enum Argument<'a> {
 }
 
 fn argument<'a>() -> impl Parser<'a, &'a str, Argument<'a>, Err<Rich<'a, char>>> {
+    // TODO can end up with stuff like I64(-1) followed by a command with no args ("u32"), so the suffix isn't correctly being respected in order
     choice((
         identifier().map(Argument::Identifier),
         unsigned_integer_with_suffix("u32".to_owned()).map(Argument::U32),
@@ -111,9 +112,172 @@ fn argument<'a>() -> impl Parser<'a, &'a str, Argument<'a>, Err<Rich<'a, char>>>
 }
 
 #[derive(Debug)]
-struct Instruction<'a> {
-    command: &'a str,
-    arguments: Vec<Argument<'a>>,
+enum Register32 {
+    R1,
+    R2,
+    R3,
+    R4,
+    R5,
+    R6,
+    R7,
+    R8,
+}
+
+impl<'a> FromStr for Register32 {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "r1" => Ok(Register32::R1),
+            "r2" => Ok(Register32::R2),
+            "r3" => Ok(Register32::R3),
+            "r4" => Ok(Register32::R4),
+            "r5" => Ok(Register32::R5),
+            "r6" => Ok(Register32::R6),
+            "r7" => Ok(Register32::R7),
+            "r8" => Ok(Register32::R8),
+            _ => Err(format!("not a valid 32-bit register: {}", s)),
+        }
+    }
+}
+
+impl<'a> TryFrom<&Argument<'a>> for Register32 {
+    type Error = String;
+
+    fn try_from(value: &Argument<'a>) -> Result<Self, Self::Error> {
+        match value {
+            Argument::Identifier(s) => s.parse(),
+            _ => Err(format!("not a valid 32-bit register: {:?}", value)),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Register64 {
+    R12,
+    R34,
+    R56,
+    R78,
+}
+
+impl<'a> FromStr for Register64 {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "r12" => Ok(Register64::R12),
+            "r34" => Ok(Register64::R34),
+            "r56" => Ok(Register64::R56),
+            "r78" => Ok(Register64::R78),
+            _ => Err(format!("not a valid 64-bit register: {}", s)),
+        }
+    }
+}
+
+impl<'a> TryFrom<&Argument<'a>> for Register64 {
+    type Error = String;
+
+    fn try_from(value: &Argument<'a>) -> Result<Self, Self::Error> {
+        match value {
+            Argument::Identifier(s) => s.parse(),
+            _ => Err(format!("not a valid 64-bit register: {:?}", value)),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum RegisterOrLiteral32 {
+    Register(Register32),
+    U32(u32),
+    I32(i32),
+}
+
+impl<'a> TryFrom<&Argument<'a>> for RegisterOrLiteral32 {
+    type Error = String;
+
+    fn try_from(value: &Argument<'a>) -> Result<Self, Self::Error> {
+        match value {
+            Argument::Identifier(identifier) => {
+                identifier.parse().map(RegisterOrLiteral32::Register)
+            }
+            Argument::I32(NumberLiteral {
+                value,
+                had_type_suffix: _,
+            }) => Ok(RegisterOrLiteral32::I32(*value)),
+            Argument::U32(NumberLiteral {
+                value,
+                had_type_suffix: _,
+            }) => Ok(RegisterOrLiteral32::U32(*value)),
+            _ => Err(format!("not a valid 32-bit argument: {:?}", value)),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum RegisterOrLiteral64 {
+    Register(Register64),
+    U64(u64),
+    I64(i64),
+}
+
+impl<'a> TryFrom<&Argument<'a>> for RegisterOrLiteral64 {
+    type Error = String;
+
+    fn try_from(value: &Argument<'a>) -> Result<Self, Self::Error> {
+        match value {
+            Argument::Identifier(identifier) => {
+                identifier.parse().map(RegisterOrLiteral64::Register)
+            }
+            Argument::I32(NumberLiteral {
+                value,
+                had_type_suffix: true,
+            }) => Err(format!(
+                "invalid 32-bit literal where 64-bit expected: {:?}",
+                value
+            )),
+            Argument::I32(NumberLiteral {
+                value,
+                had_type_suffix: false,
+            }) => Ok(RegisterOrLiteral64::I64(*value as i64)),
+            Argument::U32(NumberLiteral {
+                value,
+                had_type_suffix: true,
+            }) => Err(format!(
+                "invalid 32-bit literal where 64-bit expected: {:?}",
+                value
+            )),
+            Argument::U32(NumberLiteral {
+                value,
+                had_type_suffix: false,
+            }) => Ok(RegisterOrLiteral64::U64(*value as u64)),
+            Argument::I64(NumberLiteral {
+                value,
+                had_type_suffix: _,
+            }) => Ok(RegisterOrLiteral64::I64(*value)),
+            Argument::U64(NumberLiteral {
+                value,
+                had_type_suffix: _,
+            }) => Ok(RegisterOrLiteral64::U64(*value)),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Instruction<'a> {
+    Add32 {
+        destination: Register32,
+        left: RegisterOrLiteral32,
+        right: RegisterOrLiteral32,
+    },
+    Add64 {
+        destination: Register64,
+        left: RegisterOrLiteral64,
+        right: RegisterOrLiteral64,
+    },
+    Unknown {
+        command: &'a str,
+        arguments: Vec<Argument<'a>>,
+    },
 }
 
 fn instruction<'a>() -> impl Parser<'a, &'a str, Instruction<'a>, Err<Rich<'a, char>>> {
@@ -126,9 +290,38 @@ fn instruction<'a>() -> impl Parser<'a, &'a str, Instruction<'a>, Err<Rich<'a, c
                 .collect::<Vec<_>>()
                 .or_not(),
         )
-        .map(|(command, arguments)| Instruction {
-            command,
-            arguments: arguments.unwrap_or(vec![]),
+        .map(|(command, arguments)| (command, arguments.unwrap_or(vec![])))
+        .validate(|(command, arguments), _, emitter| {
+            match (command, arguments.as_slice()) {
+                ("add", [destination, left, right]) => {
+                    if let Ok(destination) = destination.try_into()
+                        && let Ok(left) = left.try_into()
+                        && let Ok(right) = right.try_into()
+                    {
+                        Instruction::Add32 {
+                            destination,
+                            left,
+                            right,
+                        }
+                    } else if let Ok(destination) = destination.try_into()
+                        && let Ok(left) = left.try_into()
+                        && let Ok(right) = right.try_into()
+                    {
+                        Instruction::Add64 {
+                            destination,
+                            left,
+                            right,
+                        }
+                    } else {
+                        // TODO emit error to emitter
+                        Instruction::Unknown { command, arguments }
+                    }
+                }
+                _ => {
+                    // TODO emit error to emitter
+                    Instruction::Unknown { command, arguments }
+                }
+            }
         })
 }
 
@@ -159,12 +352,8 @@ fn program<'a>() -> impl Parser<'a, &'a str, Vec<Statement<'a>>, Err<Rich<'a, ch
 fn main() {
     let input = r"
         main:
-            foo  1 ,  foo, -42i64
-
-        loop:
-            add x, x, 1
-            cmp x, 42
-            jne loop
+            add r1, r2, 42
+            add r34, r34, -1
     "
     .to_string();
 
