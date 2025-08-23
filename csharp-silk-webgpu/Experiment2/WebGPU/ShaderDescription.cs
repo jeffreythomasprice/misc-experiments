65dd7913bc141091f017f4dba5734fcf96ed5601
Jeff Price <jeffrey.thomas.price@gmail.com>
2025-08-23 19:39:01 -0400

    .

---
+++ csharp-silk-webgpu/Experiment2/WebGPU/ShaderDescription.cs
@@ -0,0 +1,6 @@
+public class ShaderDescription
+{
+	public required string Source { get; init; }
+	public required string VertexEntryPoint { get; init; }
+	public required string FragmentEntryPoint { get; init; }
+}
