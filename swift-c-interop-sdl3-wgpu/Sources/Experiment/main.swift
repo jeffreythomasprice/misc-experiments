901e1be8a9ef184f6d19db1a4f959dfd9aac59ac
Jeff Price <jeffrey.thomas.price@gmail.com>
2025-09-07 05:40:58 -0400

    .

--- swift-c-interop-sdl3-wgpu/Sources/Experiment/main.swift
+++ swift-c-interop-sdl3-wgpu/Sources/Experiment/main.swift
@@ -16,6 +16,18 @@ extension String {
 		buf[other.length] = 0
 		return String(cString: buf)
 	}
+
+	public func toWGPUStringView() -> WGPUStringView {
+		let utf8 = self.utf8
+		let count = utf8.count
+		let buf = UnsafeMutablePointer<CChar>.allocate(capacity: count)
+		var index = 0
+		for byte in utf8 {
+			buf[index] = CChar(byte)
+			index += 1
+		}
+		return WGPUStringView(data: buf, length: count)
+	}
 }
 
 @MainActor
@@ -25,19 +37,33 @@ func createWGPUInstance() -> WGPUInstance {
 	return result
 }
 
+enum CreateWGPUSurfaceError: Error {
+	case FailedToGetWindow
+	case FailedToGetWindowsModule
+	case FailedToGetDisplay
+	case UnsupportedPlatform
+}
+
 @MainActor
-func createWGPUSurface(sdlWindow: OpaquePointer, wgpuInstance: WGPUInstance) -> WGPUSurface {
+func createWGPUSurface(sdlWindow: OpaquePointer, wgpuInstance: WGPUInstance) -> Result<
+	WGPUSurface, CreateWGPUSurfaceError
+> {
 	// implement for other platforms
 	// https://github.com/eliemichel/sdl3webgpu/blob/main/sdl3webgpu.c
 
 	let props = SDL_GetWindowProperties(sdlWindow)
 
 	#if os(Windows)
-		let hwnd = SDL_GetPointerProperty(props, SDL_PROP_WINDOW_WIN32_HWND_POINTER, nil)
-		assert(hwnd != nil)
+		guard let hwnd = SDL_GetPointerProperty(props, SDL_PROP_WINDOW_WIN32_HWND_POINTER, nil)
+		else {
+			print("failed to get win32 hwnd")
+			return .failure(.FailedToGetWindow)
+		}
 
-		let hInstance = GetModuleHandleA(nil)
-		assert(hInstance != nil)
+		guard let hInstance = GetModuleHandleA(nil) else {
+			print("failed to get hinstance")
+			return .failure(.FailedToGetWindowsModule)
+		}
 
 		var fromWindowsHWND = WGPUSurfaceSourceWindowsHWND()
 		fromWindowsHWND.chain.sType = WGPUSType_SurfaceSourceWindowsHWND
@@ -58,26 +84,31 @@ func createWGPUSurface(sdlWindow: OpaquePointer, wgpuInstance: WGPUInstance) -> 
 
 			switch platform {
 			case "x11":
-				let x11Display = SDL_GetPointerProperty(
-					props, SDL_PROP_WINDOW_X11_DISPLAY_POINTER, nil)
-				let x11Window = SDL_GetNumberProperty(props, SDL_PROP_WINDOW_X11_WINDOW_NUMBER, 0)
-				if x11Display == nil || x11Window == 0 {
-					print("failed to get x11 display or window")
-					result = nil
-				} else {
-					var fromXLibWindow = WGPUSurfaceSourceXlibWindow()
-					fromXLibWindow.chain.sType = WGPUSType_SurfaceSourceXlibWindow
-					fromXLibWindow.chain.next = nil
-					fromXLibWindow.display = x11Display
-					fromXLibWindow.window = UInt64(x11Window)
+				guard
+					let x11Display = SDL_GetPointerProperty(
+						props, SDL_PROP_WINDOW_X11_DISPLAY_POINTER, nil)
+				else {
+					print("failed to get x11 display")
+					return .failure(.FailedToGetDisplay)
+				}
+				let x11Window = SDL_GetNumberProperty(
+					props, SDL_PROP_WINDOW_X11_WINDOW_NUMBER, 0)
+				guard x11Window != 0 else {
+					print("failed to get x11 window")
+					return .failure(.FailedToGetWindow)
+				}
+				var fromXLibWindow = WGPUSurfaceSourceXlibWindow()
+				fromXLibWindow.chain.sType = WGPUSType_SurfaceSourceXlibWindow
+				fromXLibWindow.chain.next = nil
+				fromXLibWindow.display = x11Display
+				fromXLibWindow.window = UInt64(x11Window)
 
-					result = withUnsafePointer(to: &fromXLibWindow.chain) {
-						fromXLibWindowChainPtr in
-						var surfaceDescriptor = WGPUSurfaceDescriptor()
-						surfaceDescriptor.nextInChain = fromXLibWindowChainPtr
-						surfaceDescriptor.label = WGPUStringView(data: nil, length: 0)
-						return wgpuInstanceCreateSurface(wgpuInstance, &surfaceDescriptor)
-					}
+				result = withUnsafePointer(to: &fromXLibWindow.chain) {
+					fromXLibWindowChainPtr in
+					var surfaceDescriptor = WGPUSurfaceDescriptor()
+					surfaceDescriptor.nextInChain = fromXLibWindowChainPtr
+					surfaceDescriptor.label = WGPUStringView(data: nil, length: 0)
+					return wgpuInstanceCreateSurface(wgpuInstance, &surfaceDescriptor)
 				}
 			case "wayland":
 				print("TODO support wayland")
@@ -100,19 +131,28 @@ func createWGPUSurface(sdlWindow: OpaquePointer, wgpuInstance: WGPUInstance) -> 
 				*/
 				result = nil
 			default:
-				print("unsupported video platform")
-				result = nil
+				print("unsupported video platform: \(platform)")
+				return .failure(.UnsupportedPlatform)
 			}
 		#endif
 	#endif
 
-	assert(result != nil)
-	print("created wgpu surface")
-	return result!
+	guard let result = result else {
+		print("failed to create wgpu surface")
+		return .failure(.FailedToGetWindow)
+	}
+	return .success(result)
+}
+
+enum CreateWGPUAdapterError: Error {
+	case FailedToCreateAdapter
+	case FailedToGetAdapterInfo
 }
 
 @MainActor
-func createWGPUAdapter(wgpuInstance: WGPUInstance, wgpuSurface: WGPUSurface) -> WGPUAdapter {
+func createWGPUAdapter(wgpuInstance: WGPUInstance, wgpuSurface: WGPUSurface) -> Result<
+	WGPUAdapter, CreateWGPUAdapterError
+> {
 	var adapterOptions = WGPURequestAdapterOptions()
 	adapterOptions.powerPreference = WGPUPowerPreference_HighPerformance
 	adapterOptions.backendType = WGPUBackendType_Vulkan
@@ -137,11 +177,16 @@ func createWGPUAdapter(wgpuInstance: WGPUInstance, wgpuSurface: WGPUSurface) -> 
 		// assert(wgpuInstanceWaitAny(wgpuInstance, 1, &futureWaitInfo, 5000) == WGPUWaitStatus_Success)
 	}
 
-	assert(result != nil)
-	print("created wgpu adapter")
+	guard let result = result else {
+		print("created wgpu adapter")
+		return .failure(.FailedToCreateAdapter)
+	}
 
 	var adapterInfo = WGPUAdapterInfo()
-	assert(wgpuAdapterGetInfo(result!, &adapterInfo) == WGPUStatus_Success)
+	guard wgpuAdapterGetInfo(result, &adapterInfo) == WGPUStatus_Success else {
+		print("failed to get adapter info")
+		return .failure(.FailedToGetAdapterInfo)
+	}
 	print("adapter type: \(adapterInfo.adapterType)")
 	print("adapter architecture: \(String.fromWGPUStringView(other: adapterInfo.architecture))")
 	print("adapter backend type: \(adapterInfo.backendType)")
@@ -151,10 +196,14 @@ func createWGPUAdapter(wgpuInstance: WGPUInstance, wgpuSurface: WGPUSurface) -> 
 	print("adapter vendor ID: \(adapterInfo.vendorID)")
 	print("adapter vendor name: \(String.fromWGPUStringView(other: adapterInfo.vendor))")
 
-	return result!
+	return .success(result)
 }
 
-func createWGPUDevice(wgpuAdapter: WGPUAdapter) -> WGPUDevice {
+enum CreateWGPUDeviceError: Error {
+	case FailedToCreateDevice
+}
+
+func createWGPUDevice(wgpuAdapter: WGPUAdapter) -> Result<WGPUDevice, CreateWGPUDeviceError> {
 	var callbackInfo = WGPURequestDeviceCallbackInfo()
 	var result: WGPUDevice? = nil
 	withUnsafeMutablePointer(to: &result) {
@@ -174,16 +223,175 @@ func createWGPUDevice(wgpuAdapter: WGPUAdapter) -> WGPUDevice {
 		// assert(wgpuInstanceWaitAny(wgpuInstance, 1, &futureWaitInfo, 5000) == WGPUWaitStatus_Success)
 	}
 
-	assert(result != nil)
+	guard let result = result else {
+		print("failed to create wgpu device")
+		return .failure(.FailedToCreateDevice)
+	}
 	print("created wgpu device")
-	return result!
+	return .success(result)
 }
 
-func createWGPUQueue(wgpuDevice: WGPUDevice) -> WGPUQueue {
+enum CreateWGPUShaderModuleError: Error {
+	case FailedToCreateShaderModule
+}
+
+func createWGPUShaderModuleWGSL(wgpuDevice: WGPUDevice, shaderSource: String) -> Result<
+	WGPUShaderModule, CreateWGPUShaderModuleError
+> {
+	var wgpuShaderSource = WGPUShaderSourceWGSL(
+		chain: WGPUChainedStruct(next: nil, sType: WGPUSType_ShaderSourceWGSL),
+		code: shaderSource.toWGPUStringView())
+	let result = withUnsafePointer(to: &wgpuShaderSource.chain) { wgpuShaderSourcePtr in
+		var shaderModuleDescriptor = WGPUShaderModuleDescriptor()
+		shaderModuleDescriptor.nextInChain = wgpuShaderSourcePtr
+		return wgpuDeviceCreateShaderModule(wgpuDevice, &shaderModuleDescriptor)
+	}
+	guard let result = result else {
+		print("failed to create wgpu shader module")
+		return .failure(.FailedToCreateShaderModule)
+	}
+	print("created wgpu shader module")
+	return .success(result)
+}
+
+enum CreateWGPUQueueError: Error {
+	case FailedToCreateQueue
+}
+
+func createWGPUQueue(wgpuDevice: WGPUDevice) -> Result<WGPUQueue, CreateWGPUQueueError> {
 	let result = wgpuDeviceGetQueue(wgpuDevice)
-	assert(result != nil)
-	print("got wgpu queue")
-	return result!
+	guard let result = result else {
+		print("failed to create wgpu queue")
+		return .failure(.FailedToCreateQueue)
+	}
+	print("created wgpu queue")
+	return .success(result)
+}
+
+enum GetWGPUSurfaceCapabilitiesError: Error {
+	case FailedToGetSurfaceCapabilities
+}
+
+func getWGPUSurfaceCapabilities(wgpuSurface: WGPUSurface, wgpuAdapter: WGPUAdapter)
+	-> Result<WGPUSurfaceCapabilities, GetWGPUSurfaceCapabilitiesError>
+{
+	var surfaceCapabilities = WGPUSurfaceCapabilities()
+	let status = wgpuSurfaceGetCapabilities(wgpuSurface, wgpuAdapter, &surfaceCapabilities)
+	guard status == WGPUStatus_Success else {
+		print("failed to get wgpu surface capabilities, status: \(status)")
+		return .failure(.FailedToGetSurfaceCapabilities)
+	}
+	print("got wgpu surface capabilities")
+	return .success(surfaceCapabilities)
+}
+
+enum CreateWGPUPipelineLayoutError: Error {
+	case FailedToCreatePipelineLayout
+}
+
+func createWGPUPipelineLayout(wgpuDevice: WGPUDevice) -> Result<
+	WGPUPipelineLayout, CreateWGPUPipelineLayoutError
+> {
+	let descriptor = WGPUPipelineLayoutDescriptor()
+	let result = withUnsafePointer(to: descriptor) { descriptorPtr in
+		wgpuDeviceCreatePipelineLayout(wgpuDevice, descriptorPtr)
+	}
+	guard let result = result else {
+		print("failed to create wgpu pipeline layout")
+		return .failure(.FailedToCreatePipelineLayout)
+	}
+	print("created wgpu pipeline layout")
+	return .success(result)
+}
+
+enum CreateWGPURenderPipelineError: Error {
+	case FailedToCreateRenderPipeline
+}
+
+func createWGPURenderPipeline(
+	wgpuDevice: WGPUDevice, wgpuPipelineLayout: WGPUPipelineLayout,
+	wgpuShaderModule: WGPUShaderModule,
+	colorTargetFormat: WGPUTextureFormat
+)
+	-> Result<WGPURenderPipeline, CreateWGPURenderPipelineError>
+{
+	var descriptor = WGPURenderPipelineDescriptor()
+
+	descriptor.layout = wgpuPipelineLayout
+
+	descriptor.vertex.module = wgpuShaderModule
+	descriptor.vertex.entryPoint = "vs_main".toWGPUStringView()
+	let vertexAttributes = [
+		// TODO something smarter for how vertex attributes work
+		WGPUVertexAttribute(
+			format: WGPUVertexFormat_Float32x2, offset: 0, shaderLocation: 0),
+		WGPUVertexAttribute(
+			format: WGPUVertexFormat_Float32x4, offset: 2 * 4, shaderLocation: 1),
+	]
+	var vertexBufferLayout = WGPUVertexBufferLayout()
+	vertexBufferLayout.stepMode = WGPUVertexStepMode_Vertex
+	// TODO something smarter for how vertex attributes work
+	vertexBufferLayout.arrayStride = (2 + 4) * 4
+	vertexBufferLayout.attributeCount = vertexAttributes.count
+	vertexAttributes.withUnsafeBufferPointer { bufferPtr in
+		vertexBufferLayout.attributes = bufferPtr.baseAddress
+		withUnsafePointer(to: &vertexBufferLayout) { vertexBufferLayoutPtr in
+			descriptor.vertex.bufferCount = 1
+			descriptor.vertex.buffers = vertexBufferLayoutPtr
+		}
+	}
+
+	var fragment = WGPUFragmentState()
+	fragment.module = wgpuShaderModule
+	fragment.entryPoint = "fs_main".toWGPUStringView()
+	var colorTargetState = WGPUColorTargetState()
+	colorTargetState.format = colorTargetFormat
+	colorTargetState.writeMask = WGPUColorWriteMask_All
+	let fragmentTargets = [colorTargetState]
+	fragment.targetCount = fragmentTargets.count
+	fragmentTargets.withUnsafeBufferPointer { bufferPtr in
+		fragment.targets = bufferPtr.baseAddress
+		withUnsafePointer(to: &fragment) { fragmentPtr in
+			descriptor.fragment = fragmentPtr
+		}
+	}
+
+	descriptor.primitive.topology = WGPUPrimitiveTopology_TriangleList
+
+	descriptor.multisample.count = 1
+	descriptor.multisample.mask = 0xFFFF_FFFF
+
+	guard let result = wgpuDeviceCreateRenderPipeline(wgpuDevice, &descriptor) else {
+		print("failed to create wgpu render pipeline")
+		return .failure(.FailedToCreateRenderPipeline)
+	}
+	print("created wgpu render pipeline")
+	return .success(result)
+}
+
+func createWGPUSurfaceConfiguration(
+	wgpuDevice: WGPUDevice, textureFormat: WGPUTextureFormat, alphaMode: WGPUCompositeAlphaMode
+) -> WGPUSurfaceConfiguration {
+	var result = WGPUSurfaceConfiguration()
+	result.device = wgpuDevice
+	result.usage = WGPUTextureUsage_RenderAttachment
+	result.format = textureFormat
+	result.presentMode = WGPUPresentMode_Fifo
+	result.alphaMode = alphaMode
+	return result
+}
+
+func resizeWGPUSurfaceConfiguration(
+	wgpuSurface: inout WGPUSurface,
+	wgpuSurfaceConfiguration: inout WGPUSurfaceConfiguration, sdlWindow: OpaquePointer
+) {
+	var width: Int32 = 0
+	var height: Int32 = 0
+	SDL_GetWindowSize(sdlWindow, &width, &height)
+	wgpuSurfaceConfiguration.width = UInt32(width)
+	wgpuSurfaceConfiguration.height = UInt32(height)
+	wgpuSurfaceConfigure(wgpuSurface, &wgpuSurfaceConfiguration)
+	print("resized wgpu surface configuration to \(width)x\(height)")
 }
 
 print("TODO add result = \(add(1,2))")
@@ -201,8 +409,9 @@ print("SDL version = \(SDL_GetVersion())")
 
 // let SDL_WINDOW_OPENGL = SDL_WindowFlags(0x0000_0000_0000_0002)
 let SDL_WINDOW_VULKAN = SDL_WindowFlags(0x0000_0000_1000_0000)
-let sdlWindow = SDL_CreateWindow("Experiment", 1024, 768, SDL_WINDOW_VULKAN)
-if sdlWindow == nil {
+let SDL_WINDOW_RESIZABLE = SDL_WindowFlags(0x0000_0000_0000_0020)
+let sdlWindow = SDL_CreateWindow("Experiment", 1024, 768, SDL_WINDOW_VULKAN | SDL_WINDOW_RESIZABLE)
+guard sdlWindow != nil else {
 	let error = String(cString: SDL_GetError())
 	print("failed to create SDL window: \(error)")
 	exit(1)
@@ -212,23 +421,33 @@ defer {
 }
 
 let wgpuInstance = createWGPUInstance()
-let wgpuSurface = createWGPUSurface(sdlWindow: sdlWindow!, wgpuInstance: wgpuInstance)
-let wgpuAdapter = createWGPUAdapter(wgpuInstance: wgpuInstance, wgpuSurface: wgpuSurface)
-let wgpuDevice = createWGPUDevice(wgpuAdapter: wgpuAdapter)
-let wgpuQueue = createWGPUQueue(wgpuDevice: wgpuDevice)
-// TODO rest of wgpu stuff, device, etc.
-// https://github.com/gfx-rs/wgpu-native/blob/trunk/examples/triangle/main.c
+var wgpuSurface = try createWGPUSurface(sdlWindow: sdlWindow!, wgpuInstance: wgpuInstance).get()
+let wgpuAdapter = try createWGPUAdapter(wgpuInstance: wgpuInstance, wgpuSurface: wgpuSurface).get()
+let wgpuDevice = try createWGPUDevice(wgpuAdapter: wgpuAdapter).get()
+let wgpuQueue = try createWGPUQueue(wgpuDevice: wgpuDevice).get()
 
-// print("TODO test 1 = \(Bundle.paths(forResourcesOfType: "wsgl", inDirectory: "../../assets"))")
-// print("TODO test 2 = \(Bundle.path(forResource: "shader", ofType: "wsgl", inDirectory: "assets"))")
-for bundle in Bundle.allBundles {
-	print("bundle = \(bundle)")
-	print("bundle identifier = \(String(describing: bundle.bundleIdentifier))")
-	print("bundle path = \(bundle.bundlePath)")
-	print("bundle resource path = \(String(describing: bundle.resourcePath))")
-	print("bundle executable path = \(String(describing: bundle.executablePath))")
-	print("bundle URL = \(bundle.bundleURL)")
-}
+let wgpuShader = try createWGPUShaderModuleWGSL(
+	wgpuDevice: wgpuDevice,
+	shaderSource: String(decoding: Data(PackageResources.shader_wsgl), as: UTF8.self)
+).get()
+
+let wgpuSurfaceCapabilities = try getWGPUSurfaceCapabilities(
+	wgpuSurface: wgpuSurface, wgpuAdapter: wgpuAdapter
+).get()
+let surfaceTextureFormat = wgpuSurfaceCapabilities.formats[0]
+
+let wgpuPipelineLayout = try createWGPUPipelineLayout(wgpuDevice: wgpuDevice).get()
+let wgpuRenderPipeline = try createWGPURenderPipeline(
+	wgpuDevice: wgpuDevice, wgpuPipelineLayout: wgpuPipelineLayout,
+	wgpuShaderModule: wgpuShader, colorTargetFormat: surfaceTextureFormat
+).get()
+
+var wgpuSurfaceConfig = createWGPUSurfaceConfiguration(
+	wgpuDevice: wgpuDevice, textureFormat: surfaceTextureFormat,
+	alphaMode: wgpuSurfaceCapabilities.alphaModes[0]
+)
+resizeWGPUSurfaceConfiguration(
+	wgpuSurface: &wgpuSurface, wgpuSurfaceConfiguration: &wgpuSurfaceConfig, sdlWindow: sdlWindow!)
 
 let framesPerSecond = 60
 let delayBetweenFrames = 1000 / framesPerSecond
@@ -251,18 +470,16 @@ while !exiting {
 			}
 			break
 		case SDL_EVENT_WINDOW_RESIZED:
-			var width: Int32 = 0
-			var height: Int32 = 0
-			SDL_GetWindowSize(sdlWindow, &width, &height)
-			print("window resized to \(width)x\(height)")
-		// TODO resize wgpu
-		//         wgpuSurfaceConfigure(demo.surface, &demo.config);
+			resizeWGPUSurfaceConfiguration(
+				wgpuSurface: &wgpuSurface, wgpuSurfaceConfiguration: &wgpuSurfaceConfig,
+				sdlWindow: sdlWindow!)
 		default:
 			break
 		}
 	}
 
 	let wgpuCommandEncoder = wgpuDeviceCreateCommandEncoder(wgpuDevice, nil)
+	// TODO no assert
 	assert(wgpuCommandEncoder != nil)
 
 	// TODO more wgpu stuff
