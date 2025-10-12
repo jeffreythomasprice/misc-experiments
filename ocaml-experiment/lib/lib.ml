0634173f65ca86e2a7f4e2ac76fd2b304bc7f6b0
Jeff Price <jeffrey.thomas.price@gmail.com>
2025-10-12 10:52:25 -0400

    .

--- ocaml-experiment/lib/lib.ml
+++ ocaml-experiment/lib/lib.ml
@@ -8,33 +8,50 @@ let string_uncons s =
 
 let is_whitespace c = c = ' ' || c = '\t' || c = '\n' || c = '\r'
 
-let rec all_elements_equal inputs =
-  match inputs with
-  | [] -> true
-  | [ _ ] -> true
-  | a :: b :: c -> a = b && all_elements_equal (b :: c)
-
-let rec trim_common_leading_whitespace inputs =
-  let heads, remainders = inputs |> List.map string_uncons |> List.split in
-
-  let all_heads_equal = all_elements_equal heads in
-
-  let leading_head_is_whitespace =
-    match heads with
-    | [] -> false
-    | Some head :: _ -> is_whitespace head
-    | None :: _ -> false
+let dedent s =
+  let rec all_elements_equal inputs =
+    match inputs with
+    | [] -> true
+    | [ _ ] -> true
+    | a :: b :: c -> a = b && all_elements_equal (b :: c)
   in
 
-  match (heads, all_heads_equal, leading_head_is_whitespace) with
-  (* the first char is whitespace in all inputs, and it's the same whitespace character *)
-  | _, true, true -> trim_common_leading_whitespace remainders
-  (* no inputs, mismatched first character, etc. *)
-  | _ -> inputs
+  let rec longest_common_whitespace_prefix inputs =
+    let heads, remainders = inputs |> List.map string_uncons |> List.split in
 
-let dedent s =
+    let all_heads_equal = all_elements_equal heads in
+
+    let leading_head_is_whitespace =
+      match heads with
+      | [] -> false
+      | Some head :: _ -> is_whitespace head
+      | None :: _ -> false
+    in
+
+    match (heads, all_heads_equal, leading_head_is_whitespace) with
+    (* the first char is whitespace in all inputs, and it's the same whitespace character *)
+    | Some head :: _, true, true ->
+        String.make 1 head ^ longest_common_whitespace_prefix remainders
+    (* no inputs, mismatched first character, etc. *)
+    | _ -> ""
+  in
+
+  let trim_prefix s prefix =
+    if String.length prefix <= String.length s then
+      String.sub s (String.length prefix)
+        (String.length s - String.length prefix)
+    else s
+  in
+
   let lines = String.split_on_char '\n' s in
-  (* TODO this needs to turn all entirely whitespace strings into empty string *)
-  (* TODO and then when trimming leading whitespace, only trim the ones that aren't already all whitespace *)
-  let trimmed = trim_common_leading_whitespace lines in
-  String.concat "\n" trimmed
+  let non_empty_lines =
+    lines |> List.filter (fun line -> String.trim line <> "")
+  in
+  let leading_whitespace = longest_common_whitespace_prefix non_empty_lines in
+  let trimmed_lines =
+    lines
+    |> List.map (fun line ->
+           let trimmed = String.trim line in
+           if trimmed == "" then "" else trim_prefix line leading_whitespace)
+  in
+  String.concat "\n" trimmed_lines
--- ocaml-experiment/test/test.ml
+++ ocaml-experiment/test/test.ml
@@ -13,30 +13,22 @@ let is_whitespace_tests =
               assert_equal expected (is_whitespace input)
                 ~printer:string_of_bool))
 
-let trim_common_leading_whitespace_tests =
-  "trim common whitespace"
+let dedent_tests =
+  "dedent"
   >::: ([
-          ( "has common whitespace",
-            [ " foo"; "  bar"; "   baz" ],
-            [ "foo"; " bar"; "  baz" ] );
+          ("has common whitespace", " foo\n  bar\n   baz", "foo\n bar\n  baz");
           ( "has common whitespace, 2",
-            [ "\t  \tfoo"; "\t bar"; "\t \t\t\tbaz" ],
-            [ " \tfoo"; "bar"; "\t\t\tbaz" ] );
-          ( "no common prefix",
-            [ "\t  \tfoo"; "bar"; " baz" ],
-            [ "\t  \tfoo"; "bar"; " baz" ] );
+            "\t  \tfoo\n\t bar\n\t \t\t\tbaz",
+            " \tfoo\nbar\n\t\t\tbaz" );
+          ("no common prefix", "\t  \tfoo\nbar\n baz", "\t  \tfoo\nbar\n baz");
           ( "non-whitespace common prefix",
-            [ "aaafoo"; "aaabar"; "aaabaz" ],
-            [ "aaafoo"; "aaabar"; "aaabaz" ] );
+            "aaafoo\naaabar\naaabaz",
+            "aaafoo\naaabar\naaabaz" );
         ]
-       |> List.map (fun (name, inputs, expected) ->
+       |> List.map (fun (name, input, expected) ->
               name >:: fun _ ->
-              let actual = trim_common_leading_whitespace inputs in
+              let actual = dedent input in
               assert_equal expected actual))
 
-(* TODO dedent tests *)
-
-let tests =
-  "tests" >::: [ is_whitespace_tests; trim_common_leading_whitespace_tests ]
-
+let tests = "tests" >::: [ is_whitespace_tests; dedent_tests ]
 let _ = run_test_tt_main tests
