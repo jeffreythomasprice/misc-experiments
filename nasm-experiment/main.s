f9df2cd79ca8d32a279a27f606407e376e83940d
Jeff Price <jeffrey.thomas.price@gmail.com>
2025-11-27 06:28:27 -0500

    .

---
+++ nasm-experiment/main.s
@@ -0,0 +1,20 @@
+bits 64
+
+section .text
+
+global _start
+_start:
+	mov     rax, 1 ; write
+	mov     rdi, 1 ; stdout
+	mov     rsi, msg
+	mov     rdx, msg.len
+	syscall
+
+	mov     rax, 60 ; exit
+	mov     rdi, 0
+	syscall
+
+section .data
+
+msg: db "Hello, world!"
+.len: equ $ - msg
